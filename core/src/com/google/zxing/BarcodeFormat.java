4a39981245cf9f32b2e519dcf4f74ed000b8d0b0
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2007-11-06 19:07:08 +0000

    More javadoc
    git-svn-id: https://zxing.googlecode.com/svn/trunk@10 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/BarcodeFormat.java
+++ core/src/com/google/zxing/BarcodeFormat.java
@@ -25,8 +25,13 @@ public final class BarcodeFormat {
 
   // No, we can't use an enum here. J2ME doesn't support it.
 
+  /** UPC 1D barcode format family. */
   public static final BarcodeFormat UPC = new BarcodeFormat();
+  /** QR Code 2D barcode format */
   public static final BarcodeFormat QR_CODE = new BarcodeFormat();
+  /** DataMatrix 2D barcode format */
   public static final BarcodeFormat DATAMATRIX = new BarcodeFormat();
+    
+  private BarcodeFormat() {}
 
 }
