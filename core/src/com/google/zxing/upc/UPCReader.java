3adc22a5d9c0c5048eb611397da35262a9921ae9
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-01-07 21:17:40 +0000

    Removed unneeded import
    git-svn-id: http://zxing.googlecode.com/svn/trunk@137 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/upc/UPCReader.java
+++ core/src/com/google/zxing/upc/UPCReader.java
@@ -20,7 +20,6 @@ import com.google.zxing.MonochromeBitmapSource;
 import com.google.zxing.Reader;
 import com.google.zxing.ReaderException;
 import com.google.zxing.Result;
-import com.google.zxing.ResultPoint;
 
 import java.util.Hashtable;
 
