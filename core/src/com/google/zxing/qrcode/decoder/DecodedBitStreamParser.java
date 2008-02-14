7862288d74820f26945b645614d7141e191f1c60
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-02-14 19:42:59 +0000

    Another bug fix
    git-svn-id: http://zxing.googlecode.com/svn/trunk@188 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
+++ core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
@@ -217,7 +217,7 @@ final class DecodedBitStreamParser {
         int nextValue = bytes[i + 1] & 0xFF;
         if ((value & 0x1) == 0) {
           // if even,
-          if (nextValue >= 0x9F && nextValue <= 0x7C) {
+          if (nextValue >= 0x9F && nextValue <= 0xFC) {
             return SHIFT_JIS;
           }
         } else {
