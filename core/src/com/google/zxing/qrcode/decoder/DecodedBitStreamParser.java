bbe423c7c2837515626f0b29ea0e0bdaaf260f30
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-02-14 19:41:33 +0000

    Oops fixed bug in guessing Shift_JIS -- flipped if condition!
    git-svn-id: https://zxing.googlecode.com/svn/trunk@187 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
+++ core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
@@ -217,11 +217,11 @@ final class DecodedBitStreamParser {
         int nextValue = bytes[i + 1] & 0xFF;
         if ((value & 0x1) == 0) {
           // if even,
-          if (nextValue >= 0x40 && nextValue <= 0x9E) {
+          if (nextValue >= 0x9F && nextValue <= 0x7C) {
             return SHIFT_JIS;
           }
         } else {
-          if (nextValue >= 0x9F && nextValue <= 0x7C) {
+          if (nextValue >= 0x40 && nextValue <= 0x9E) {
             return SHIFT_JIS;
           }
         }
