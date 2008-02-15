4c4adc953ebe86baaf7501c16b518e4f3450e15b
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-02-15 17:51:11 +0000

    Brought this back into sync with BufferedImageMonochromeBitmapSource
    git-svn-id: https://zxing.googlecode.com/svn/trunk@205 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/YUVMonochromeBitmapSource.java
+++ android/src/com/google/zxing/client/android/YUVMonochromeBitmapSource.java
@@ -34,9 +34,6 @@ import com.google.zxing.common.BlackPointEstimator;
 final class YUVMonochromeBitmapSource implements MonochromeBitmapSource {
 
   private final Bitmap image;
-  private final BitArray[] blackWhitePixels;
-  private final int width;
-  private final int height;
   private int blackPoint;
   private BlackPointEstimationMethod lastMethod;
   private int lastArgument;
@@ -46,108 +43,68 @@ final class YUVMonochromeBitmapSource implements MonochromeBitmapSource {
   private static final int LUMINANCE_BUCKETS = 1 << LUMINANCE_BITS;
 
   YUVMonochromeBitmapSource(Bitmap image) {
-    width = image.width();
-    height = image.height();
     this.image = image;
-    blackWhitePixels = new BitArray[height];
     blackPoint = 0x7F;
     lastMethod = null;
     lastArgument = 0;
   }
 
   public boolean isBlack(int x, int y) {
-    BitArray blackWhite = blackWhitePixels[y];
-    if (blackWhite == null) {
-      blackWhite = parseBlackWhite(y);
-    }
-    return blackWhite.get(x);
+    return ((image.getPixel(x, y) >> 16) & 0xFF) < blackPoint;
   }
 
   public BitArray getBlackRow(int y, BitArray row, int startX, int getWidth) {
-    BitArray blackWhite = blackWhitePixels[y];
-    if (blackWhite == null) {
-      blackWhite = parseBlackWhite(y);
-    }
     if (row == null) {
-      if (startX == 0 && getWidth == width) {
-        return blackWhite;
-      }
       row = new BitArray(getWidth);
     } else {
       row.clear();
     }
+    int[] pixelRow = new int[getWidth];
+     image.getPixels(pixelRow, 0, getWidth, startX, y, getWidth, 1);
     for (int i = 0; i < getWidth; i++) {
-      if (blackWhite.get(startX + i)) {
+      if (((pixelRow[i] >> 16) & 0xFF) < blackPoint) {
         row.set(i);
       }
     }
     return row;
   }
 
-  private BitArray parseBlackWhite(int y) {
-    int width = this.width;
-    int[] pixelRow = new int[width];
-    image.getPixels(pixelRow, 0, width, 0, y, width, 1);
-    BitArray luminanceRow = new BitArray(width);
-    int blackPoint = this.blackPoint;
-    // Calculate 32 bits at a time to more efficiently set the bit array
-    int bits = 0;
-    int bitCount = 0;
-    for (int j = 0; j < width; j++) {
-      bits >>>= 1;
-      // Computation of luminance is inlined here for speed:      
-      if (((pixelRow[j] >> 16) & 0xFF) <= blackPoint) {
-        bits |= 0x80000000;
-      }
-      if (++bitCount == 32) {
-        luminanceRow.setBulk(j, bits);
-        bits = 0;
-        bitCount = 0;
-      }
-    }
-    if (bitCount > 0) {
-      luminanceRow.setBulk(width, bits >>> (32 - bitCount));
-    }
-    blackWhitePixels[y] = luminanceRow;
-    return luminanceRow;
-  }
-
   public int getHeight() {
-    return height;
+    return image.height();
   }
 
   public int getWidth() {
-    return width;
+    return image.width();
   }
 
   public void estimateBlackPoint(BlackPointEstimationMethod method, int argument) {
     if (!method.equals(lastMethod) || argument != lastArgument) {
-      for (int i = 0; i < blackWhitePixels.length; i++) {
-        blackWhitePixels[i] = null;
-      }
+      int width = image.width();
+      int height = image.height();
       int[] histogram = new int[LUMINANCE_BUCKETS];
+      float biasTowardsWhite = 1.0f;
       if (method.equals(BlackPointEstimationMethod.TWO_D_SAMPLING)) {
         int minDimension = width < height ? width : height;
         int startI = height == minDimension ? 0 : (height - width) >> 1;
         int startJ = width == minDimension ? 0 : (width - height) >> 1;
         for (int n = 0; n < minDimension; n++) {
           int pixel = image.getPixel(startJ + n, startI + n);
-          // Computation of luminance is inlined here for speed:
           histogram[((pixel >> 16) & 0xFF) >> LUMINANCE_SHIFT]++;
         }
       } else if (method.equals(BlackPointEstimationMethod.ROW_SAMPLING)) {
         if (argument < 0 || argument >= height) {
           throw new IllegalArgumentException("Row is not within the image: " + argument);
         }
-        int[] yuvArray = new int[width];
-        image.getPixels(yuvArray, 0, width, 0, argument, width, 1);
+        biasTowardsWhite = 2.0f;
+        int[] pixelRow = new int[width];
+        image.getPixels(pixelRow, 0, width, 0, argument, width, 1);
         for (int x = 0; x < width; x++) {
-          histogram[((yuvArray[x] >> 16) & 0xFF) >> LUMINANCE_SHIFT]++;
+          histogram[((pixelRow[x] >> 16) & 0xFF) >> LUMINANCE_SHIFT]++;
         }
       } else {
         throw new IllegalArgumentException("Unknown method: " + method);
       }
-      blackPoint = BlackPointEstimator.estimate(histogram, 1.0f) << LUMINANCE_SHIFT;
+      blackPoint = BlackPointEstimator.estimate(histogram, biasTowardsWhite) << LUMINANCE_SHIFT;
       lastMethod = method;
       lastArgument = argument;
     }
