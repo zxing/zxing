bcdd87c32f9f7a32935bc282f61f9eabb1606998
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-03-31 19:04:38 +0000

    Made the RGB to luminance approximation/optimization a little faster -- one less shift
    git-svn-id: http://zxing.googlecode.com/svn/trunk@327 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android-m3/src/com/google/zxing/client/android/RGBMonochromeBitmapSource.java
+++ android-m3/src/com/google/zxing/client/android/RGBMonochromeBitmapSource.java
@@ -140,9 +140,9 @@ final class RGBMonochromeBitmapSource implements MonochromeBitmapSource {
     // corrupts the conversion. Not significant for our purposes.
     //
     // But we can get even cleverer and eliminate a few shifts:
-    return (((pixel & 0x00FF0000) >> 8)  +
-            ((pixel & 0x0000FF00) << 1) +
-            ((pixel & 0x000000FF) << 8)) >> 10;
+    return (((pixel & 0x00FF0000) >> 16)  +
+            ((pixel & 0x0000FF00) >>  7) +
+            ( pixel & 0x000000FF       )) >> 2;
   }
 
 }
