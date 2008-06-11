31e75a402dae9aca99556e661029f812b3a55cf9
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-06-11 19:30:57 +0000

    Unify UPC-A reader into EAN-13 reader since EAN-13 is a superset and we can take advantage of the commonality to improve performance.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@416 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/oned/MultiFormatUPCEANReader.java
+++ core/src/com/google/zxing/oned/MultiFormatUPCEANReader.java
@@ -40,8 +40,7 @@ public final class MultiFormatUPCEANReader extends AbstractOneDReader {
     if (possibleFormats != null) {
       if (possibleFormats.contains(BarcodeFormat.EAN_13)) {
         readers.addElement(new EAN13Reader());
-      }
-      if (possibleFormats.contains(BarcodeFormat.UPC_A)) {
+      } else if (possibleFormats.contains(BarcodeFormat.UPC_A)) {
         readers.addElement(new UPCAReader());
       }
       if (possibleFormats.contains(BarcodeFormat.EAN_8)) {
@@ -52,8 +51,8 @@ public final class MultiFormatUPCEANReader extends AbstractOneDReader {
       }
     }
     if (readers.isEmpty()) {
-      readers.addElement(new EAN13Reader());      
-      readers.addElement(new UPCAReader());
+      readers.addElement(new EAN13Reader());
+      // UPC-A is covered by EAN-13
       readers.addElement(new EAN8Reader());
       readers.addElement(new UPCEReader());
     }
@@ -71,16 +70,14 @@ public final class MultiFormatUPCEANReader extends AbstractOneDReader {
       // Special case: a 12-digit code encoded in UPC-A is identical to a "0"
       // followed by those 12 digits encoded as EAN-13. Each will recognize such a code,
       // UPC-A as a 12-digit string and EAN-13 as a 13-digit string starting with "0".
-      // Individually these are correct.
+      // Individually these are correct and their readers will both read such a code
+      // and correctly call it EAN-13, or UPC-A, respectively.
       //
-      // These cases can't be distinguished, so we defer to the UPC-A decoder and
-      // treat this case as a UPC-A code. But if we let the UPC-A decoder look at
-      // symbols first, it will recognize any EAN-13 code as a 12-digit string,
-      // which is wrong. So EAN-13 has to try first.
-      //
-      // Here is, therefore, where we implement this logic:
-      if (result.getBarcodeFormat().equals(BarcodeFormat.EAN_13) &&
-          result.getText().charAt(0) == '0') {
+      // In this case, if we've been looking for both types, we'd like to call it
+      // a UPC-A code. But for efficiency we only run the EAN-13 decoder to also read
+      // UPC-A. So we special case it here, and convert an EAN-13 result to a UPC-A
+      // result if appropriate.
+      if (result.getBarcodeFormat().equals(BarcodeFormat.EAN_13) && result.getText().charAt(0) == '0') {
         return new Result(result.getText().substring(1), null, result.getResultPoints(), BarcodeFormat.UPC_A);
       }
       return result;
