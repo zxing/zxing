8c9498c1a6d1ab51e7df2017caafb2b190f1675e
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-06-25 00:51:37 +0000

    Reworked the false positives unit tests to track each rotation separately, and to allow new tests to be added. I also removed four images which are going to live in another test.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@475 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/test/src/com/google/zxing/common/FalsePositivesBlackBoxTestCase.java
+++ core/test/src/com/google/zxing/common/FalsePositivesBlackBoxTestCase.java
@@ -16,85 +16,21 @@
 
 package com.google.zxing.common;
 
-import com.google.zxing.MonochromeBitmapSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.ReaderException;
-import com.google.zxing.Result;
-import com.google.zxing.client.j2se.BufferedImageMonochromeBitmapSource;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 /**
- * This test ensures that random, noisy, or unsupported barcode images do not decode.
+ * This test ensures that random images with high contrast patterns do not decode as barcodes.
  *
  * @author dswitkin@google.com (Daniel Switkin)
  */
-public final class FalsePositivesBlackBoxTestCase extends AbstractBlackBoxTestCase {
+public final class FalsePositivesBlackBoxTestCase extends AbstractNegativeBlackBoxTestCase {
 
-  // This number should be reduced as we get better at rejecting false positives.
-  private static final int FALSE_POSITIVES_ALLOWED = 4;
-
-  // Use the multiformat reader to evaluate all decoders in the system.
   public FalsePositivesBlackBoxTestCase() {
-    super(new File("test/data/blackbox/falsepositives"), new MultiFormatReader(), null);
-  }
-
-  @Override
-  public void testBlackBox() throws IOException {
-    File[] imageFiles = getImageFiles();
-    int falsePositives = 0;
-    for (File testImage : imageFiles) {
-      System.out.println("Starting " + testImage.getAbsolutePath());
-
-      // Try all four rotations, since many of the test images don't have a notion of up, and we
-      // want to be as robust as possible.
-      BufferedImage image = ImageIO.read(testImage);
-      if (image == null) {
-        throw new IOException("Could not read image: " + testImage);
-      }
-      for (int x = 0; x < 4; x++) {
-        if (!checkForFalsePositives(image, x * 90.0f)) {
-          falsePositives++;
-        }
-      }
-    }
-
-    System.out.println("Found " + falsePositives + " false positives (" + FALSE_POSITIVES_ALLOWED +
-        " max)");
-    assertTrue("Too many false positives found", falsePositives <= FALSE_POSITIVES_ALLOWED);
-  }
-
-  /**
-   * Make sure ZXing does NOT find a barcode in the image.
-   *
-   * @param image The image to test
-   * @param rotationInDegrees The amount of rotation to apply
-   * @return true if nothing found, false if a non-existant barcode was detected
-   */
-  private boolean checkForFalsePositives(BufferedImage image, float rotationInDegrees) {
-    BufferedImage rotatedImage = rotateImage(image, rotationInDegrees);
-    MonochromeBitmapSource source = new BufferedImageMonochromeBitmapSource(rotatedImage);
-    Result result;
-    try {
-      result = getReader().decode(source);
-      System.out.println("Found false positive: '" + result.getText() + "' with format '" +
-          result.getBarcodeFormat() + "' (rotation: " + rotationInDegrees + ')');
-      return false;
-    } catch (ReaderException re) {
-    }
-
-    // Try "try harder" mode
-    try {
-      result = getReader().decode(source, TRY_HARDER_HINT);
-      System.out.println("Try harder found false positive: '" + result.getText() + "' with format '" +
-          result.getBarcodeFormat() + "' (rotation: " + rotationInDegrees + ')');
-      return false;
-    } catch (ReaderException re) {
-    }
-    return true;
+    super(new File("test/data/blackbox/falsepositives"));
+    addTest(1, 0.0f);
+    addTest(1, 90.0f);
+    addTest(1, 180.0f);
+    addTest(0, 270.0f);
   }
 
 }
