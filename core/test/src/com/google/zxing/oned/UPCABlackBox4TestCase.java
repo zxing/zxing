1ce4a98557d99ace838c65201a9bb2ceef10c228
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-10-01 14:17:17 +0000

    Added two more UPC-A blackbox images submitted by the community.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@593 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/test/src/com/google/zxing/oned/UPCABlackBox4TestCase.java
+++ core/test/src/com/google/zxing/oned/UPCABlackBox4TestCase.java
@@ -29,8 +29,8 @@ public final class UPCABlackBox4TestCase extends AbstractBlackBoxTestCase {
 
   public UPCABlackBox4TestCase() {
     super(new File("test/data/blackbox/upca-4"), new MultiFormatReader(), BarcodeFormat.UPC_A);
-    addTest(0, 1, 0.0f);
-    addTest(0, 1, 180.0f);
+    addTest(1, 2, 0.0f);
+    addTest(1, 2, 180.0f);
   }
 
 }
