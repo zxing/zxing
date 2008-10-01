acd2742d51bf4ab013a016f935b02a108a2c485e
christian.brunschen <christian.brunschen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-10-01 20:19:55 +0000

    Resurrected 128-pixel icon for About page Removed UI autorotation support, since UIImagePickerController doe not gracefully handle being showin in non-portrait orientation and there is not at the moment a supported way to force the UI into a specified orientation.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@595 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- iphone/Classes/DecoderViewController.m
+++ iphone/Classes/DecoderViewController.m
@@ -226,14 +226,14 @@
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	// Return YES for supported orientations
-	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+  // Return YES for supported orientations
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-	// Release anything that's not essential, such as cached data
+  [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+  // Release anything that's not essential, such as cached data
 }
 
 - (void)dealloc {
--- iphone/Classes/MessageViewController.m
+++ iphone/Classes/MessageViewController.m
@@ -60,7 +60,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
 	// Return YES for supported orientations
-	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 - (void)didReceiveMemoryWarning {
