0d7dfdbfae1ac994623fa9681d6ab01b753e43fb
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-10-13 16:08:58 +0000

    Added the missing AndroidHttpClient to the package, which fixes the Android client build using the 1.0 SDK.
    git-svn-id: https://zxing.googlecode.com/svn/trunk@613 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/android/barcodes/QRCodeEncoder.java
+++ android/src/com/android/barcodes/QRCodeEncoder.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
