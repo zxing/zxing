bbbef0d7166c01a30f8cf5cfd8caf73c0043a07c
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-11-12 15:42:44 +0000

    Wrote a new bookmark picker activity for use by the Share button, because I couldn't get the platform version to work.
    git-svn-id: https://zxing.googlecode.com/svn/trunk@691 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ android/src/com/google/zxing/client/android/BookmarkPickerActivity.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.client.android;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+/**
+ * This class is only needed because I can't successfully send an ACTION_PICK intent to
+ * com.android.browser.BrowserBookmarksPage. It can go away if that starts working in the future.
+ */
+public class BookmarkPickerActivity extends ListActivity {
+
+  private static final String[] BOOKMARK_PROJECTION = {
+      Browser.BookmarkColumns.TITLE,
+      Browser.BookmarkColumns.URL
+  };
+
+  private static final int[] TWO_LINE_VIEW_IDS = {
+      R.id.bookmark_title,
+      R.id.bookmark_url
+  };
+
+  private static final int TITLE_COLUMN = 0;
+  private static final int URL_COLUMN = 1;
+
+  // Without this selection, we'd get all the history entries too
+  private static final String BOOKMARK_SELECTION = "bookmark = 1";
+
+  private Cursor mCursor;
+
+  @Override
+  protected void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+
+    mCursor = getContentResolver().query(Browser.BOOKMARKS_URI, BOOKMARK_PROJECTION,
+        BOOKMARK_SELECTION, null, null);
+    startManagingCursor(mCursor);
+
+    ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.bookmark_picker_list_item,
+        mCursor, BOOKMARK_PROJECTION, TWO_LINE_VIEW_IDS);
+    setListAdapter(adapter);
+  }
+
+  @Override
+  protected void onListItemClick(ListView l, View view, int position, long id) {
+    if (mCursor.moveToPosition(position)) {
+      Intent intent = new Intent();
+      intent.putExtra(Browser.BookmarkColumns.TITLE, mCursor.getString(TITLE_COLUMN));
+      intent.putExtra(Browser.BookmarkColumns.URL, mCursor.getString(URL_COLUMN));
+      setResult(RESULT_OK, intent);
+    } else {
+      setResult(RESULT_CANCELED);
+    }
+    finish();
+  }
+
+}
