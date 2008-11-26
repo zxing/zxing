76ef7062cb2b69f76ab4efa871af521d6173770f
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2008-11-26 16:39:34 +0000

    Added a new QR Code unit test of real world photos of t-shirts. These are notoriously difficult because the barcodes don't lie completely flat. We're able to decode 33/48 which is better than I thought, although most of the images which fail look within reach.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@764 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ core/test/src/com/google/zxing/qrcode/QRCodeBlackBox4TestCase.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.qrcode;
+
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.AbstractBlackBoxTestCase;
+
+import java.io.File;
+
+/**
+ * Tests of various QR Codes from t-shirts, which are notoriously not flat.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+public final class QRCodeBlackBox4TestCase extends AbstractBlackBoxTestCase {
+
+  public QRCodeBlackBox4TestCase() {
+    super(new File("test/data/blackbox/qrcode-4"), new MultiFormatReader(), BarcodeFormat.QR_CODE);
+    addTest(33, 33, 0.0f);
+    addTest(33, 33, 90.0f);
+    addTest(32, 32, 180.0f);
+    addTest(32, 32, 270.0f);
+  }
+
+}
