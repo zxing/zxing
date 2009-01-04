95194bc27f3734bc2b86bd84363ad7e712b22d75
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-01-04 21:11:25 +0000

    On a tip from Sandy found an error in the QR code detector that would miss a third finder pattern in cases of about 135 degree rotation. This changes test results but is a net win.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@814 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/detector/FinderPatternFinder.java
+++ core/src/com/google/zxing/qrcode/detector/FinderPatternFinder.java
@@ -423,10 +423,10 @@ final class FinderPatternFinder {
           // How far down can we skip before resuming looking for the next
           // pattern? In the worst case, only the difference between the
           // difference in the x / y coordinates of the two centers.
-          // This is the case where you find top left first. Draw it out.
+          // This is the case where you find top left last.
           hasSkipped = true;
           return (int) (Math.abs(firstConfirmedCenter.getX() - center.getX()) -
-              Math.abs(firstConfirmedCenter.getY() - center.getY()));
+              Math.abs(firstConfirmedCenter.getY() - center.getY())) / 2;
         }
       }
     }
--- core/test/src/com/google/zxing/qrcode/QRCodeBlackBox4TestCase.java
+++ core/test/src/com/google/zxing/qrcode/QRCodeBlackBox4TestCase.java
@@ -31,7 +31,7 @@ public final class QRCodeBlackBox4TestCase extends AbstractBlackBoxTestCase {
 
   public QRCodeBlackBox4TestCase() {
     super(new File("test/data/blackbox/qrcode-4"), new MultiFormatReader(), BarcodeFormat.QR_CODE);
-    addTest(33, 33, 0.0f);
+    addTest(32, 32, 0.0f);
     addTest(33, 33, 90.0f);
     addTest(32, 32, 180.0f);
     addTest(32, 32, 270.0f);
