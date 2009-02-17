81ef8e251ca8cfb868204c407805ccbacdac3321
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-02-17 12:27:14 +0000

    Added small android-integration module
    git-svn-id: https://zxing.googlecode.com/svn/trunk@860 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ android-integration/src/com/google/zxing/integration/android/IntentResult.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2009 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.integration.android;
+
+/**
+ * <p>Encapsulates the result of a barcode scan invoked through {@link IntentIntegrator}.</p>
+ *
+ * @author Sean Owen
+ */
+public final class IntentResult {
+
+  private final String contents;
+  private final String formatName;
+
+  IntentResult(String contents, String formatName) {
+    this.contents = contents;
+    this.formatName = formatName;
+  }
+
+  /**
+   * @return raw content of barcode
+   */
+  public String getContents() {
+    return contents;
+  }
+
+  /**
+   * @return name of format, like "QR_CODE", "UPC_A". See <code>BarcodeFormat</code> for more format names.
+   */
+  public String getFormatName() {
+    return formatName;
+  }
+
+}
