312d33c381c80e0f2042cc905c16e49850065150
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-04-13 20:33:28 +0000

    Removed a bunch of logging cruft from the network code which we don't need.
    git-svn-id: https://zxing.googlecode.com/svn/trunk@906 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/AndroidHttpClient.java
+++ android/src/com/google/zxing/client/android/AndroidHttpClient.java
@@ -19,7 +19,6 @@ package com.google.zxing.client.android;
 import android.util.Log;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpMessage;
 import org.apache.http.HttpRequest;
@@ -38,7 +37,6 @@ import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.RequestWrapper;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
@@ -52,7 +50,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -142,8 +139,6 @@ public final class AndroidHttpClient implements HttpClient {
         // Add interceptor to prevent making requests from main thread.
         BasicHttpProcessor processor = super.createHttpProcessor();
         processor.addRequestInterceptor(sThreadCheckInterceptor);
-        processor.addRequestInterceptor(new CurlLogger());
-
         return processor;
       }
 
@@ -307,130 +302,4 @@ public final class AndroidHttpClient implements HttpClient {
   private static long getMinGzipSize() {
     return DEFAULT_SYNC_MIN_GZIP_BYTES;
   }
-
-  /* cURL logging support. */
-
-  /**
-   * Logging tag and level.
-   */
-  private static final class LoggingConfiguration {
-
-    private final String tag;
-    private final int level;
-
-    private LoggingConfiguration(String tag, int level) {
-      this.tag = tag;
-      this.level = level;
-    }
-
-    /**
-     * Returns true if logging is turned on for this configuration.
-     */
-    private boolean isLoggable() {
-      return Log.isLoggable(tag, level);
-    }
-
-    /**
-     * Prints a message using this configuration.
-     */
-    private void println(String message) {
-      Log.println(level, tag, message);
-    }
-  }
-
-  /**
-   * cURL logging configuration.
-   */
-  private volatile LoggingConfiguration curlConfiguration;
-
-  /**
-   * Enables cURL request logging for this client.
-   *
-   * @param name  to log messages with
-   * @param level at which to log messages (see {@link android.util.Log})
-   */
-  public void enableCurlLogging(String name, int level) {
-    if (name == null) {
-      throw new NullPointerException("name");
-    }
-    if (level < Log.VERBOSE || level > Log.ASSERT) {
-      throw new IllegalArgumentException("Level is out of range ["
-          + Log.VERBOSE + ".." + Log.ASSERT + ']');
-    }
-
-    curlConfiguration = new LoggingConfiguration(name, level);
-  }
-
-  /**
-   * Disables cURL logging for this client.
-   */
-  public void disableCurlLogging() {
-    curlConfiguration = null;
-  }
-
-  /**
-   * Logs cURL commands equivalent to requests.
-   */
-  private final class CurlLogger implements HttpRequestInterceptor {
-    public void process(HttpRequest request, HttpContext context)
-        throws IOException {
-      LoggingConfiguration configuration = curlConfiguration;
-      if (configuration != null
-          && configuration.isLoggable()
-          && request instanceof HttpUriRequest) {
-        configuration.println(toCurl((HttpUriRequest) request));
-      }
-    }
-
-    /**
-     * Generates a cURL command equivalent to the given request.
-     */
-    private String toCurl(HttpUriRequest request) throws IOException {
-      StringBuilder builder = new StringBuilder();
-
-      builder.append("curl ");
-
-      for (Header header : request.getAllHeaders()) {
-        builder.append("--header \"");
-        builder.append(header.toString().trim());
-        builder.append("\" ");
-      }
-
-      URI uri = request.getURI();
-
-      // If this is a wrapped request, use the URI from the original
-      // request instead. getURI() on the wrapper seems to return a
-      // relative URI. We want an absolute URI.
-      if (request instanceof RequestWrapper) {
-        HttpRequest original = ((RequestWrapper) request).getOriginal();
-        if (original instanceof HttpUriRequest) {
-          uri = ((HttpUriRequest) original).getURI();
-        }
-      }
-
-      builder.append('"');
-      builder.append(uri);
-      builder.append('"');
-
-      if (request instanceof HttpEntityEnclosingRequest) {
-        HttpEntityEnclosingRequest entityRequest =
-            (HttpEntityEnclosingRequest) request;
-        HttpEntity entity = entityRequest.getEntity();
-        if (entity != null && entity.isRepeatable()) {
-          if (entity.getContentLength() < 1024) {
-            OutputStream stream = new ByteArrayOutputStream();
-            entity.writeTo(stream);
-            String entityString = stream.toString();
-            // TODO: Check the content type, too.
-            builder.append(" --data-ascii \"").append(entityString).append('"');
-          } else {
-            builder.append(" [TOO MUCH DATA TO INCLUDE]");
-          }
-        }
-      }
-
-      return builder.toString();
-    }
-  }
-
 }
