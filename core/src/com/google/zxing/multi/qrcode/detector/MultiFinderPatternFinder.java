d71a862e30b2591c1e39332acf5c3d8391c5b582
dswitkin <dswitkin@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-06-22 15:32:59 +0000

    Modified some comments to reflect that these objects are thread-safe but not reentrant.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@974 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/multi/qrcode/detector/MultiFinderPatternFinder.java
+++ core/src/com/google/zxing/multi/qrcode/detector/MultiFinderPatternFinder.java
@@ -34,8 +34,7 @@ import java.util.Vector;
  * <p>This class attempts to find finder patterns in a QR Code. Finder patterns are the square
  * markers at three corners of a QR Code.</p>
  *
- * <p>This class is not thread-safe and should not be reused.</p>
- * TODO(dswitkin): Is this comment still true? I don't see any global variables for example.
+ * <p>This class is thread-safe but not reentrant. Each thread must allocate its own object.
  *
  * <p>In contrast to {@link FinderPatternFinder}, this class will return an array of all possible
  * QR code locations in the image.</p>
