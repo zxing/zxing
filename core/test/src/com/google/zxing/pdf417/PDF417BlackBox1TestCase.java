372dfa11459b8e22fe2e54c9e9c1fb0d3fea8557
kev.sully <kev.sully@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-06-24 10:03:37 +0000

    ISSUE: http://code.google.com/p/zxing/issues/detail?id=42
    Added PDF417 to list of readers. Will be used only if its specified in a Hint.
    
    git-svn-id: https://zxing.googlecode.com/svn/trunk@984 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ core/test/src/com/google/zxing/pdf417/PDF417BlackBox1TestCase.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.pdf417;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.AbstractBlackBoxTestCase;
+
+/**
+ * @author SITA Lab (kevin.osullivan@sita.aero)
+ */
+public final class PDF417BlackBox1TestCase extends AbstractBlackBoxTestCase {
+
+  public PDF417BlackBox1TestCase() {
+    super("test/data/blackbox/pdf417", new MultiFormatReader(), BarcodeFormat.PDF417);
+    addTest(1, 1, 0.0f);
+    //addTest(1, 1, 90.0f);
+    //addTest(1, 1, 180.0f);
+    //addTest(1, 1, 270.0f);
+  }
+  
+  protected Hashtable<DecodeHintType, Object> getHints() {
+     Hashtable<DecodeHintType, Object> table = new Hashtable<DecodeHintType, Object>();
+     Vector v = new Vector();
+     v.add(BarcodeFormat.PDF417);
+     table.put(DecodeHintType.POSSIBLE_FORMATS, v);
+     
+     return table;
+   }
+
+}
