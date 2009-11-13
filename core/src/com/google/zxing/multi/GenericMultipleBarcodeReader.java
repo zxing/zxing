431ace93e17873f959ac1757b531d275904eb5f0
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2009-11-13 09:57:14 +0000

    Oops fixed typo in last patch
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1108 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/multi/GenericMultipleBarcodeReader.java
+++ core/src/com/google/zxing/multi/GenericMultipleBarcodeReader.java
@@ -129,12 +129,12 @@ public final class GenericMultipleBarcodeReader implements MultipleBarcodeReader
     }
     // Decode right of barcode
     if (maxX < width - MIN_DIMENSION_TO_RECUR) {
-      doDecodeMultiple(image.crop((int) maxX, 0, MIN_DIMENSION_TO_RECUR - (int) maxX, height),
+      doDecodeMultiple(image.crop((int) maxX, 0, width - (int) maxX, height),
                        hints, results, (int) maxX, 0);
     }
     // Decode below barcode
     if (maxY < height - MIN_DIMENSION_TO_RECUR) {
-      doDecodeMultiple(image.crop(0, (int) maxY, width, MIN_DIMENSION_TO_RECUR - (int) maxY),
+      doDecodeMultiple(image.crop(0, (int) maxY, width, height - (int) maxY),
                        hints, results, 0, (int) maxY);
     }
   }
