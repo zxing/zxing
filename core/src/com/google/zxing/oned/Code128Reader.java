c7a5b5f0a990c393acd366a396d81a297da6d032
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-01-05 09:40:54 +0000

    Issue 311 fix
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1174 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/oned/Code128Reader.java
+++ core/src/com/google/zxing/oned/Code128Reader.java
@@ -420,10 +420,11 @@ public final class Code128Reader extends OneDReader {
     // Check for ample whitespace following pattern, but, to do this we first need to remember that
     // we fudged decoding CODE_STOP since it actually has 7 bars, not 6. There is a black bar left
     // to read off. Would be slightly better to properly read. Here we just skip it:
-    while (row.get(nextStart)) {
+    int width = row.getSize();
+    while (nextStart < width && row.get(nextStart)) {
       nextStart++;
     }
-    if (!row.isRange(nextStart, Math.min(row.getSize(), nextStart + (nextStart - lastStart) / 2),
+    if (!row.isRange(nextStart, Math.min(width, nextStart + (nextStart - lastStart) / 2),
         false)) {
       throw ReaderException.getInstance();
     }
