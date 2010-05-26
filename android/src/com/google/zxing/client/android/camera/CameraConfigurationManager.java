217e7fd92d0153cf194bb39ad4ac73a0f122375f
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-05-26 07:01:58 +0000

    Safer check for Build SDK; restrict Behold II workaround to Cupcake, per Samsung
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1390 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/camera/CameraConfigurationManager.java
+++ android/src/com/google/zxing/client/android/camera/CameraConfigurationManager.java
@@ -181,7 +181,9 @@ final class CameraConfigurationManager {
   private void setFlash(Camera.Parameters parameters) {
     // FIXME: This is a hack to turn the flash off on the Samsung Galaxy.
     // And this is a hack-hack to work around a different value on the Behold II
-    if (Build.MODEL.contains("Behold II")) {
+    // Restrict Behold II check to Cupcake, per Samsung's advice
+    if (Build.MODEL.contains("Behold II") &&
+        CameraManager.SDK_INT == Build.VERSION_CODES.CUPCAKE) {
       parameters.set("flash-value", 1);
     } else {
       parameters.set("flash-value", 2);
