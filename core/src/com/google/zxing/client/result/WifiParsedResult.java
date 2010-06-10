21b7a0f3bd8fb3507b3e08aa025a0101669228d3
vikrama <vikrama@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-06-10 03:49:18 +0000

    Core library changes to include wifi type.  One measly test included as well.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1421 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ core/src/com/google/zxing/client/result/WifiParsedResult.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2010 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.client.result;
+
+/**
+ * @author Vikram Aggarwal
+ */
+public final class WifiParsedResult extends ParsedResult {
+  private final String ssid;
+  private final String networkEncryption;
+  private final String password;
+
+  public WifiParsedResult(String networkEncryption, String ssid, String password) {
+    super(ParsedResultType.WIFI);
+    this.ssid = ssid;
+    this.networkEncryption = networkEncryption;
+    this.password = password;
+  }
+
+  public String getSsid() {
+    return ssid;
+  }
+
+  public String getNetworkEncryption() {
+    return networkEncryption;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getDisplayResult() {
+    StringBuffer result = new StringBuffer(80);
+    maybeAppend(ssid, result);
+    maybeAppend(networkEncryption, result);
+    maybeAppend(password, result);
+    return result.toString();
+  }
+}
