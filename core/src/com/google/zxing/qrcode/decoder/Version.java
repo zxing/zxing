7fbdebb62feb0c54de6ef2e994e9c6ee4814ca71
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-08-16 20:19:09 +0000

    Issue 519
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1537 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/decoder/Version.java
+++ core/src/com/google/zxing/qrcode/decoder/Version.java
@@ -419,7 +419,7 @@ public final class Version {
             new ECBlocks(30, new ECB(7, 24),
                 new ECB(16, 25)),
             new ECBlocks(24, new ECB(34, 13))),
-        new Version(23, new int[]{6, 30, 54, 74, 102},
+        new Version(23, new int[]{6, 30, 54, 78, 102},
             new ECBlocks(30, new ECB(4, 121),
                 new ECB(5, 122)),
             new ECBlocks(28, new ECB(4, 47),
