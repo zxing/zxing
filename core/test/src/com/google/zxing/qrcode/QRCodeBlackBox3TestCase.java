346d1631250b18785ab6e316c8f4f6aec5a51eb3
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-08-16 21:55:33 +0000

    Issue 520
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1540 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/test/src/com/google/zxing/qrcode/QRCodeBlackBox3TestCase.java
+++ core/test/src/com/google/zxing/qrcode/QRCodeBlackBox3TestCase.java
@@ -27,10 +27,10 @@ public final class QRCodeBlackBox3TestCase extends AbstractBlackBoxTestCase {
 
   public QRCodeBlackBox3TestCase() {
     super("test/data/blackbox/qrcode-3", new MultiFormatReader(), BarcodeFormat.QR_CODE);
-    addTest(36, 36, 0.0f);
+    addTest(38, 38, 0.0f);
     addTest(38, 38, 90.0f);
     addTest(36, 36, 180.0f);
-    addTest(37, 37, 270.0f);
+    addTest(38, 38, 270.0f);
   }
 
 }
