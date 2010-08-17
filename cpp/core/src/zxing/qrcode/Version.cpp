a251967f552d6dcd395c9fad5ad3d1fffcc059d5
smparkes@smparkes.net <smparkes@smparkes.net@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-08-17 16:19:57 +0000

    Issue 519
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1543 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- cpp/core/src/zxing/qrcode/Version.cpp
+++ cpp/core/src/zxing/qrcode/Version.cpp
@@ -384,7 +384,7 @@ int Version::buildVersions() {
                                   new ECBlocks(30, new ECB(7, 24),
                                                new ECB(16, 25)),
                                   new ECBlocks(24, new ECB(34, 13)))));
-  VERSIONS.push_back(Ref<Version>(new Version(23, intArray(5, 6, 30, 54, 74, 102),
+  VERSIONS.push_back(Ref<Version>(new Version(23, intArray(5, 6, 30, 54, 78, 102),
                                   new ECBlocks(30, new ECB(4, 121),
                                                new ECB(5, 122)),
                                   new ECBlocks(28, new ECB(4, 47),
