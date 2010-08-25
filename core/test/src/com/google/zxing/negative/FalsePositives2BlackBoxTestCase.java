a26a7880879d983c8d0bbaf2f56391999d12edde
dswitkin@google.com <dswitkin@google.com@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2010-08-25 18:23:00 +0000

    Created new false positives blackbox test. We definitely have a problem with EAN_8 firing too often.
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1560 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ core/test/src/com/google/zxing/negative/FalsePositives2BlackBoxTestCase.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.negative;
+
+import com.google.zxing.common.AbstractNegativeBlackBoxTestCase;
+
+/**
+ * Additional random images with high contrast patterns which should not find any barcodes.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+public final class FalsePositives2BlackBoxTestCase extends AbstractNegativeBlackBoxTestCase {
+
+  public FalsePositives2BlackBoxTestCase() {
+    super("test/data/blackbox/falsepositives-2");
+    addTest(6, 0.0f);
+    addTest(6, 90.0f);
+    addTest(6, 180.0f);
+    addTest(6, 270.0f);
+  }
+
+}
