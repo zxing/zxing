ea813c3f88949a6d3da68a672d8f14efc078c17b
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-02-21 14:53:11 +0000

    User-contributed doc fixes and Issue 725
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1718 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/oned/ITFReader.java
+++ core/src/com/google/zxing/oned/ITFReader.java
@@ -27,10 +27,14 @@ import com.google.zxing.common.BitArray;
 import java.util.Hashtable;
 
 /**
- * <p>Implements decoding of the ITF format.</p>
+ * <p>Implements decoding of the ITF format, or Interleaved Two of Five.</p>
  *
- * <p>"ITF" stands for Interleaved Two of Five. This Reader will scan ITF barcode with 6, 10 or 14
- * digits. The checksum is optional and is not applied by this Reader. The consumer of the decoded
+ * <p>This Reader will scan ITF barcodes of certain lengths only.
+ * At the moment it reads length 6, 10, 12, 14, 16, 24, and 44 as these have appeared "in the wild". Not all
+ * lengths are scanned, especially shorter ones, to avoid false positives. This in turn is due to a lack of
+ * required checksum function.</p>
+ *
+ * <p>The checksum is optional and is not applied by this Reader. The consumer of the decoded
  * value will have to apply a checksum if required.</p>
  *
  * <p><a href="http://en.wikipedia.org/wiki/Interleaved_2_of_5">http://en.wikipedia.org/wiki/Interleaved_2_of_5</a>
