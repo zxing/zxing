47b28a4edbb3af2dc60f0366c8f1305be5adcf7f
smparkes@smparkes.net <smparkes@smparkes.net@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-05-17 02:07:56 +0000

    objective c port for iOS and OS X
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1773 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ objc/src/zxing/ZXImage.mm
@@ -0,0 +1,62 @@
+// -*- mode:objc; c-basic-offset:2; indent-tabs-mode:nil -*-
+/*
+ * Copyright 2011 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <zxing/ZXImage.h>
+
+@implementation ZXImage
+
+@synthesize cgimage;
+
+- (ZXImage*)initWithURL:(NSURL const*)url {
+  
+  CGDataProviderRef provider = CGDataProviderCreateWithURL((CFURLRef)url);
+
+  if (provider) {
+    CGImageSourceRef source = CGImageSourceCreateWithDataProvider(provider, 0);
+
+    if (source) {
+      cgimage = CGImageSourceCreateImageAtIndex(source, 0, 0);
+
+      CFRelease(source);
+    }
+
+    CGDataProviderRelease(provider);
+  }
+
+  if (cgimage) {
+    return self;
+  } else {
+    return 0;
+  }
+}
+
+- (size_t)width {
+  return CGImageGetWidth(cgimage);
+}
+
+- (size_t)height {
+  return CGImageGetHeight(cgimage);
+}
+
+- (void)dealloc {
+  if (cgimage) {
+    CGImageRelease(cgimage);
+  }
+  [super dealloc];
+}
+
+@end
---
+++ objc/src/zxing/ZXReader.h
@@ -0,0 +1,31 @@
+// -*- mode:objc; c-basic-offset:2; indent-tabs-mode:nil -*-
+/*
+ * Copyright 2011 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <zxing/Reader.h>
+
+@class ZXBinaryBitmap;
+@class ZXDecodeHints;
+@class ZXResult;
+
+@interface ZXReader : NSObject {
+  zxing::Ref<zxing::Reader> reader;
+}
+
+- (ZXReader*)initWithReader:(zxing::Reader*)reader;
+- (ZXResult*)decode:(ZXBinaryBitmap*)bitmap hints:(ZXDecodeHints*)hints;
+
+@end
