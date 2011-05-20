7fb00f0e25c4c683730d801132e7d7336c29a6be
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-05-20 07:12:49 +0000

    Issue 742 at least parse iCal GEO in core library
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1787 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/client/result/VEventResultParser.java
+++ core/src/com/google/zxing/client/result/VEventResultParser.java
@@ -20,7 +20,7 @@ import com.google.zxing.Result;
 
 /**
  * Partially implements the iCalendar format's "VEVENT" format for specifying a
- * calendar event. See RFC 2445. This supports SUMMARY, LOCATION, DTSTART and DTEND fields.
+ * calendar event. See RFC 2445. This supports SUMMARY, LOCATION, GEO, DTSTART and DTEND fields.
  *
  * @author Sean Owen
  */
@@ -44,8 +44,21 @@ final class VEventResultParser extends ResultParser {
     String end = VCardResultParser.matchSingleVCardPrefixedField("DTEND", rawText, true);
     String location = VCardResultParser.matchSingleVCardPrefixedField("LOCATION", rawText, true);
     String description = VCardResultParser.matchSingleVCardPrefixedField("DESCRIPTION", rawText, true);
+
+    String geoString = VCardResultParser.matchSingleVCardPrefixedField("GEO", rawText, true);
+    double latitude;
+    double longitude;
+    if (geoString == null) {
+      latitude = Double.NaN;
+      longitude = Double.NaN;
+    } else {
+      int semicolon = geoString.indexOf(';');
+      latitude = Double.parseDouble(geoString.substring(0, semicolon));
+      longitude = Double.parseDouble(geoString.substring(semicolon + 1));
+    }
+
     try {
-      return new CalendarParsedResult(summary, start, end, location, null, description);
+      return new CalendarParsedResult(summary, start, end, location, null, description, latitude, longitude);
     } catch (IllegalArgumentException iae) {
       return null;
     }
