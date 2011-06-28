04e54ad7bb7192abc7990b483369dda44c6fba85
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-06-28 06:50:37 +0000

    Avoid AIOOBE for corrupt codes
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1837 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
+++ core/src/com/google/zxing/qrcode/decoder/DecodedBitStreamParser.java
@@ -275,6 +275,9 @@ final class DecodedBitStreamParser {
     // Read three digits at a time
     while (count >= 3) {
       // Each 10 bits encodes three digits
+      if (bits.available() < 10) {
+        throw FormatException.getFormatInstance();
+      }
       int threeDigitsBits = bits.readBits(10);
       if (threeDigitsBits >= 1000) {
         throw FormatException.getFormatInstance();
@@ -286,6 +289,9 @@ final class DecodedBitStreamParser {
     }
     if (count == 2) {
       // Two digits left over to read, encoded in 7 bits
+      if (bits.available() < 7) {
+        throw FormatException.getFormatInstance();
+      }
       int twoDigitsBits = bits.readBits(7);
       if (twoDigitsBits >= 100) {
         throw FormatException.getFormatInstance();
@@ -294,6 +300,9 @@ final class DecodedBitStreamParser {
       result.append(toAlphaNumericChar(twoDigitsBits % 10));
     } else if (count == 1) {
       // One digit left over to read
+      if (bits.available() < 4) {
+        throw FormatException.getFormatInstance();
+      }
       int digitBits = bits.readBits(4);
       if (digitBits >= 10) {
         throw FormatException.getFormatInstance();
