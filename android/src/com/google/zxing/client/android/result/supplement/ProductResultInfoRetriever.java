4e98b15baab73fc8359c86b2c989144855dc9c18
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-08-22 12:08:33 +0000

    Issue 941 retrieve more info from Google Books for ISBNs
    git-svn-id: http://zxing.googlecode.com/svn/trunk@1891 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/result/supplement/ProductResultInfoRetriever.java
+++ android/src/com/google/zxing/client/android/result/supplement/ProductResultInfoRetriever.java
@@ -18,27 +18,17 @@ package com.google.zxing.client.android.result.supplement;
 
 import android.content.Context;
 import android.os.Handler;
-import android.util.Log;
 import android.widget.TextView;
+import com.google.zxing.client.android.R;
 import com.google.zxing.client.android.history.HistoryManager;
-import com.google.zxing.client.android.AndroidHttpClient;
 import com.google.zxing.client.android.LocaleManager;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 final class ProductResultInfoRetriever extends SupplementalInfoRetriever {
-
-  private static final String TAG = ProductResultInfoRetriever.class.getSimpleName();
 
   private static final String BASE_PRODUCT_URI =
       "http://www.google." + LocaleManager.getProductSearchCountryTLD() +
@@ -48,14 +38,16 @@ final class ProductResultInfoRetriever extends SupplementalInfoRetriever {
 
 
   private final String productID;
+  private final String source;
 
   ProductResultInfoRetriever(TextView textView,
                              String productID,
                              Handler handler,
                              HistoryManager historyManager,
                              Context context) {
-    super(textView, handler, historyManager, context);
+    super(textView, handler, historyManager);
     this.productID = productID;
+    this.source = context.getString(R.string.msg_google_product);
   }
 
   @Override
@@ -63,52 +55,11 @@ final class ProductResultInfoRetriever extends SupplementalInfoRetriever {
 
     String encodedProductID = URLEncoder.encode(productID, "UTF-8");
     String uri = BASE_PRODUCT_URI + encodedProductID;
+    String content = downloadViaHttp(uri);
 
-    HttpUriRequest head = new HttpGet(uri);
-    AndroidHttpClient client = AndroidHttpClient.newInstance(null);
-    HttpResponse response = client.execute(head);
-    int status = response.getStatusLine().getStatusCode();
-    if (status != 200) {
-      return;
-    }
-
-    String content = consume(response.getEntity());
     Matcher matcher = PRODUCT_NAME_PRICE_PATTERN.matcher(content);
     if (matcher.find()) {
-      String details = matcher.group(1) + ' ' + matcher.group(2);
-      append(productID, details);
-    }
-    setLink(uri);
-  }
-
-  private static String consume(HttpEntity entity) {
-    Log.d(TAG, "Consuming entity");
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    InputStream in = null;
-    try {
-      in = entity.getContent();
-      byte[] buffer = new byte[1024];
-      int bytesRead;
-      while ((bytesRead = in.read(buffer)) > 0) {
-        out.write(buffer, 0, bytesRead);
-      }
-    } catch (IOException ioe) {
-      Log.w(TAG, ioe);
-      // continue
-    } finally {
-      if (in != null) {
-        try {
-          in.close();
-        } catch (IOException ioe) {
-          // continue
-        }
-      }
-    }
-    try {
-      return new String(out.toByteArray(), "UTF-8");
-    } catch (UnsupportedEncodingException uee) {
-      // can't happen
-      throw new IllegalStateException(uee);
+      append(productID, source, new String[] { matcher.group(1), matcher.group(2) }, uri);
     }
   }
 
