177e8728c9b953ee80cd31461ad06e85411eb2f5
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-09-01 08:51:04 +0000

    Add a couple helpful methods
    git-svn-id: https://zxing.googlecode.com/svn/trunk@1899 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/LuminanceSource.java
+++ core/src/com/google/zxing/LuminanceSource.java
@@ -110,4 +110,28 @@ public abstract class LuminanceSource {
     throw new RuntimeException("This luminance source does not support rotation.");
   }
 
+  public String toString() {
+    byte[] row = new byte[width];
+    StringBuffer result = new StringBuffer(height * (width + 1));
+    for (int y = 0; y < height; y++) {
+      row = getRow(y, row);
+      for (int x = 0; x < width; x++) {
+        int luminance = row[x] & 0xFF;
+        char c;
+        if (luminance < 0x40) {
+          c = '#';
+        } else if (luminance < 0x80) {
+          c = '+';
+        } else if (luminance < 0xC0) {
+          c = '.';
+        } else {
+          c = ' ';
+        }
+        result.append(c);
+      }
+      result.append('\n');
+    }
+    return result.toString();
+  }
+
 }
