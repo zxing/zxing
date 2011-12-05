b257b12ad65e1ba1b644419ec04842037f42fdf6
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-12-05 20:51:31 +0000

    Issue 393 add intent extra to control delay after scan by intent
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2065 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/Intents.java
+++ android/src/com/google/zxing/client/android/Intents.java
@@ -87,6 +87,12 @@ public final class Intents {
     public static final String HEIGHT = "SCAN_HEIGHT";
 
     /**
+     * Desired duration in milliseconds for which to pause after a successful scan before
+     * returning to the calling intent. Specified as a long.
+     */
+    public static final String RESULT_DISPLAY_DURATION_MS = "RESULT_DISPLAY_DURATION_MS";
+
+    /**
      * If a barcode is found, Barcodes returns RESULT_OK to 
      * {@link android.app.Activity#onActivityResult(int, int, android.content.Intent)}
      * of the app which requested the scan via
