f2fe9dbd673c6550166daa74d1e642694b38cac0
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2011-12-06 20:13:27 +0000

    Added more metadata to the integrator
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2068 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android-integration/src/com/google/zxing/integration/android/IntentResult.java
+++ android-integration/src/com/google/zxing/integration/android/IntentResult.java
@@ -26,15 +26,23 @@ public final class IntentResult {
   private final String contents;
   private final String formatName;
   private final byte[] rawBytes;
+  private final Integer orientation;
+  private final String errorCorrectionLevel;
 
   IntentResult() {
-    this(null, null, null);
+    this(null, null, null, null, null);
   }
 
-  IntentResult(String contents, String formatName, byte[] rawBytes) {
+  IntentResult(String contents,
+               String formatName,
+               byte[] rawBytes,
+               Integer orientation,
+               String errorCorrectionLevel) {
     this.contents = contents;
     this.formatName = formatName;
     this.rawBytes = rawBytes;
+    this.orientation = orientation;
+    this.errorCorrectionLevel = errorCorrectionLevel;
   }
 
   /**
@@ -56,6 +64,32 @@ public final class IntentResult {
    */
   public byte[] getRawBytes() {
     return rawBytes;
+  }
+
+  /**
+   * @return rotation of the image, in degrees, which resulted in a successful scan. May be null.
+   */
+  public Integer getOrientation() {
+    return orientation;
+  }
+
+  /**
+   * @return name of the error correction level used in the barcode, if applicable
+   */
+  public String getErrorCorrectionLevel() {
+    return errorCorrectionLevel;
+  }
+  
+  @Override
+  public String toString() {
+    StringBuilder dialogText = new StringBuilder(100);
+    dialogText.append("Format: ").append(formatName).append('\n');
+    dialogText.append("Contents: ").append(contents).append('\n');
+    int rawBytesLength = rawBytes == null ? 0 : rawBytes.length;
+    dialogText.append("Raw bytes: (").append(rawBytesLength).append(" bytes)\n");
+    dialogText.append("Orientation: ").append(orientation).append('\n');
+    dialogText.append("EC level: ").append(errorCorrectionLevel).append('\n');
+    return dialogText.toString();
   }
 
 }
