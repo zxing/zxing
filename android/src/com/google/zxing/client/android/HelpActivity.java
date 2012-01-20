1ccc7a249f634ad8bcbf79da13b3ea39234ec123
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-01-20 19:43:58 +0000

    Remove defunct 'buggy' message for 4.0 from HelpActivity. And add the other half of new History impl that I forgot
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2125 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/HelpActivity.java
+++ android/src/com/google/zxing/client/android/HelpActivity.java
@@ -17,13 +17,9 @@
 package com.google.zxing.client.android;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.KeyEvent;
 import android.webkit.WebView;
@@ -37,18 +33,6 @@ import android.widget.Button;
  */
 public final class HelpActivity extends Activity {
 
-  private static final String TAG = HelpActivity.class.getSimpleName();
-
-  // Actually guessing at the Desire's MODEL for now:
-  private static final String[] BUGGY_MODEL_SUBSTRINGS = {
-      "Desire",
-      "Pulse", // Camera doesn't come on
-      "Geeksphone", // Doesn't support YUV?
-      "supersonic", // aka Evo
-  };
-  private static final Uri BUGGY_URI =
-      Uri.parse("http://code.google.com/p/zxing/wiki/FrequentlyAskedQuestions");
-
   // Use this key and one of the values below when launching this activity via intent. If not
   // present, the default page will be loaded.
   public static final String REQUESTED_PAGE_KEY = "requested_page_key";
@@ -59,7 +43,6 @@ public final class HelpActivity extends Activity {
       "file:///android_asset/html-" + LocaleManager.getTranslatedAssetLanguage() + '/';
   private static final String WEBVIEW_STATE_PRESENT = "webview_state_present";
 
-  private static boolean initialized = false;
   private WebView webView;
   private Button backButton;
 
@@ -74,16 +57,6 @@ public final class HelpActivity extends Activity {
     @Override
     public void onClick(View view) {
       finish();
-    }
-  };
-
-  private final DialogInterface.OnClickListener groupsListener =
-      new DialogInterface.OnClickListener() {
-    @Override
-    public void onClick(DialogInterface dialogInterface, int i) {
-      Intent intent = new Intent(Intent.ACTION_VIEW, BUGGY_URI);
-      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-      HelpActivity.this.startActivity(intent);
     }
   };
 
@@ -116,28 +89,6 @@ public final class HelpActivity extends Activity {
     backButton.setOnClickListener(backListener);
     View doneButton = findViewById(R.id.done_button);
     doneButton.setOnClickListener(doneListener);
-
-    if (!initialized) {
-      initialized = true;
-      checkBuggyDevice();
-    }
-  }
-
-  private void checkBuggyDevice() {
-    String model = Build.MODEL;
-    Log.i(TAG, "Build model is " + model);
-    if (model != null) {
-      for (String buggyModelSubstring : BUGGY_MODEL_SUBSTRINGS) {
-        if (model.contains(buggyModelSubstring)) {
-          AlertDialog.Builder builder = new AlertDialog.Builder(this);
-          builder.setMessage(R.string.msg_buggy);
-          builder.setPositiveButton(R.string.button_ok, groupsListener);
-          builder.setNegativeButton(R.string.button_cancel, null);
-          builder.show();
-          break;
-        }
-      }
-    }
   }
 
   @Override
