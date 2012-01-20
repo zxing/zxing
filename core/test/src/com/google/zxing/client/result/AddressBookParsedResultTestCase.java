4c7720a8a65a179b6cf91cc1252ce1f55a253296
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-01-20 16:45:39 +0000

    Update generator version and add one more test
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2123 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/test/src/com/google/zxing/client/result/AddressBookParsedResultTestCase.java
+++ core/test/src/com/google/zxing/client/result/AddressBookParsedResultTestCase.java
@@ -88,6 +88,18 @@ public final class AddressBookParsedResultTestCase extends Assert {
            null, null, null, null, null, null);
   }
 
+  @Test
+  public void testVCardEscape() {
+    doTest("BEGIN:VCARD\r\nNOTE:foo\\nbar\r\nEND:VCARD",
+           null, null, null, null, null, null, null, null, null, "foo\nbar");
+    doTest("BEGIN:VCARD\r\nNOTE:foo\\;bar\r\nEND:VCARD",
+               null, null, null, null, null, null, null, null, null, "foo;bar");
+    doTest("BEGIN:VCARD\r\nNOTE:foo\\\\bar\r\nEND:VCARD",
+                   null, null, null, null, null, null, null, null, null, "foo\\bar");
+    doTest("BEGIN:VCARD\r\nNOTE:foo\\,bar\r\nEND:VCARD",
+                       null, null, null, null, null, null, null, null, null, "foo,bar");
+  }
+
   private static void doTest(String contents,
                              String title,
                              String[] names,
