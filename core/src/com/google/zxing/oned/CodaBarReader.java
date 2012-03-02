a88da9d00257c798f8fc8a9b351fc1e828dd9aab
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-03-02 21:38:34 +0000

    Issue 1194 small bug fix in array expanding comparisons
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2212 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/oned/CodaBarReader.java
+++ core/src/com/google/zxing/oned/CodaBarReader.java
@@ -249,7 +249,7 @@ public final class CodaBarReader extends OneDReader {
   private void counterAppend(int e) {
     counters[counterLength] = e;
     counterLength++;
-    if (counterLength >= counterLength) {
+    if (counterLength >= counters.length) {
       int[] temp = new int[counterLength * 2];
       System.arraycopy(counters, 0, temp, 0, counterLength);
       counters = temp;
