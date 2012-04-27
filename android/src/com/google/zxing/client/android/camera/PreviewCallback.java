9a94f713dbf86fd184997bbe26cf79198c027139
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-04-27 11:27:37 +0000

    Look out for early preview frame before complete configuration
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2273 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/camera/PreviewCallback.java
+++ android/src/com/google/zxing/client/android/camera/PreviewCallback.java
@@ -43,13 +43,13 @@ final class PreviewCallback implements Camera.PreviewCallback {
   public void onPreviewFrame(byte[] data, Camera camera) {
     Point cameraResolution = configManager.getCameraResolution();
     Handler thePreviewHandler = previewHandler;
-    if (thePreviewHandler != null) {
+    if (cameraResolution != null && thePreviewHandler != null) {
       Message message = thePreviewHandler.obtainMessage(previewMessage, cameraResolution.x,
           cameraResolution.y, data);
       message.sendToTarget();
       previewHandler = null;
     } else {
-      Log.d(TAG, "Got preview callback, but no handler for it");
+      Log.d(TAG, "Got preview callback, but no handler or resolution available");
     }
   }
 
