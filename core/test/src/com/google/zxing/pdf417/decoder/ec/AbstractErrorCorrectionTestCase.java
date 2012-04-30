9a168f59945fa4aaa613e4e924ce6208750c10de
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-04-30 18:43:40 +0000

    Forgot tests for PDF417 EC
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2278 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ core/test/src/com/google/zxing/pdf417/decoder/ec/AbstractErrorCorrectionTestCase.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.zxing.pdf417.decoder.ec;
+
+import org.junit.Assert;
+
+import java.security.SecureRandom;
+import java.util.BitSet;
+import java.util.Random;
+
+/**
+ * @author Sean Owen
+ */
+abstract class AbstractErrorCorrectionTestCase extends Assert {
+
+  static void corrupt(int[] received, int howMany, Random random) {
+    BitSet corrupted = new BitSet(received.length);
+    for (int j = 0; j < howMany; j++) {
+      int location = random.nextInt(received.length);
+      if (corrupted.get(location)) {
+        j--;
+      } else {
+        corrupted.set(location);
+        received[location] = random.nextInt(929);
+      }
+    }
+  }
+
+  static Random getRandom() {
+    return new SecureRandom(new byte[] {(byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF});
+  }
+
+  static void assertArraysEqual(int[] expected,
+                                int expectedOffset,
+                                int[] actual,
+                                int actualOffset,
+                                int length) {
+    for (int i = 0; i < length; i++) {
+      assertEquals(expected[expectedOffset + i], actual[actualOffset + i]);
+    }
+  }
+
+}
