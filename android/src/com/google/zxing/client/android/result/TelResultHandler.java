4a907a6201ab1cd4b1a2bff44f1e137032e03ca1
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-05-03 07:39:40 +0000

    Running underneath the dialer doesn't work. Quit the app if leaving to dial a number.
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2282 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/result/TelResultHandler.java
+++ android/src/com/google/zxing/client/android/result/TelResultHandler.java
@@ -54,6 +54,10 @@ public final class TelResultHandler extends ResultHandler {
     switch (index) {
       case 0:
         dialPhoneFromUri(telResult.getTelURI());
+        // When dialer comes up, it allows underlying display activity to continue or something,
+        // but app can't get camera in this state. Avoid issues by just quitting, only in the
+        // case of a phone number
+        getActivity().finish();
         break;
       case 1:
         String[] numbers = new String[1];
