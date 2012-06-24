585318cb22d4cd165ecd1017514c1a7687fb8243
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-06-24 15:44:36 +0000

    Issue 1295 allow broader definition of  URI
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2318 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/client/result/URIResultParser.java
+++ core/src/com/google/zxing/client/result/URIResultParser.java
@@ -28,23 +28,20 @@ import java.util.regex.Pattern;
  */
 public final class URIResultParser extends ResultParser {
 
-  private static final String PATTERN_END = 
-      "(:\\d{1,5})?" + // maybe port
-      "(/|\\?|$)"; // query, path or nothing
-  private static final Pattern URL_WITH_PROTOCOL_PATTERN = Pattern.compile(
-      "[a-zA-Z0-9]{2,}:(/)*" + // protocol
-      "[a-zA-Z0-9\\-]+(\\.[a-zA-Z0-9\\-]+)*" + // host name elements
-      PATTERN_END);
+  private static final String ALPHANUM_PART = "[a-zA-Z0-9\\-]";
+  private static final Pattern URL_WITH_PROTOCOL_PATTERN = Pattern.compile("[a-zA-Z0-9]{2,}:");
   private static final Pattern URL_WITHOUT_PROTOCOL_PATTERN = Pattern.compile(
-      "([a-zA-Z0-9\\-]+\\.)+[a-zA-Z0-9\\-]{2,}" + // host name elements
-      PATTERN_END);
+      "(" + ALPHANUM_PART + "+\\.)+" + ALPHANUM_PART + "{2,}" + // host name elements
+      "(:\\d{1,5})?" + // maybe port
+      "(/|\\?|$)"); // query, path or nothing
 
   @Override
   public URIParsedResult parse(Result result) {
     String rawText = getMassagedText(result);
-    // We specifically handle the odd "URL" scheme here for simplicity
-    if (rawText.startsWith("URL:")) {
-      rawText = rawText.substring(4);
+    // We specifically handle the odd "URL" scheme here for simplicity and add "URI" for fun
+    // Assume anything starting this way really means to be a URI
+    if (rawText.startsWith("URL:") || rawText.startsWith("URI:")) {
+      return new URIParsedResult(rawText.substring(4).trim(), null);
     }
     rawText = rawText.trim();
     return isBasicallyValidURI(rawText) ? new URIParsedResult(rawText, null) : null;
