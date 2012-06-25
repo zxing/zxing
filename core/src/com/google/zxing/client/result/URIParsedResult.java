249b2e04ff7e89cefc28ec4bef41394f8a51965d
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-06-25 21:18:50 +0000

    One final simplification
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2323 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/client/result/URIParsedResult.java
+++ core/src/com/google/zxing/client/result/URIParsedResult.java
@@ -75,9 +75,6 @@ public final class URIParsedResult extends ParsedResult {
     } else if (isColonFollowedByPortNumber(uri, protocolEnd)) {
       // Found a colon, but it looks like it is after the host, so the protocol is still missing
       uri = "http://" + uri;
-    } else {
-      // Lowercase protocol to avoid problems
-      uri = uri.substring(0, protocolEnd) + uri.substring(protocolEnd);
     }
     return uri;
   }
