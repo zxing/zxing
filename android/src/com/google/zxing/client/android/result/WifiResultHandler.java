811f93b080d2d70510bef009a006e47616aa319b
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-08-06 12:07:52 +0000

    Issue 1218 add possible support for hidden SSIDs
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2371 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/result/WifiResultHandler.java
+++ android/src/com/google/zxing/client/android/result/WifiResultHandler.java
@@ -28,7 +28,8 @@ import com.google.zxing.client.result.WifiParsedResult;
 /**
  * Handles address book entries.
  *
- * @author viki@google.com (Vikram Aggarwal)
+ * @author Vikram Aggarwal
+ * @author Sean Owen
  */
 public final class WifiResultHandler extends ResultHandler {
 
@@ -52,15 +53,11 @@ public final class WifiResultHandler extends ResultHandler {
 
   @Override
   public void handleButtonPress(int index) {
-    // Get the underlying wifi config
-    WifiParsedResult wifiResult = (WifiParsedResult) getResult();
     if (index == 0) {
-      String ssid = wifiResult.getSsid();
-      String password = wifiResult.getPassword();
-      String networkType = wifiResult.getNetworkEncryption();
+      WifiParsedResult wifiResult = (WifiParsedResult) getResult();
       WifiManager wifiManager = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
       Toast.makeText(getActivity(), R.string.wifi_changing_network, Toast.LENGTH_LONG).show();
-      WifiConfigManager.configure(wifiManager, ssid, password, networkType);
+      WifiConfigManager.configure(wifiManager, wifiResult);
       parent.restartPreviewAfterDelay(0L);
     }
   }
