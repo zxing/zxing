42dcca3ccf0a726b4b83abc23ca0cc495fa2c109
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-11-20 06:48:39 +0000

    Warn about using Google product results
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2523 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/result/supplement/ProductResultInfoRetriever.java
+++ android/src/com/google/zxing/client/android/result/supplement/ProductResultInfoRetriever.java
@@ -29,6 +29,14 @@ import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * <p>Retrieves product information from Google Product search.</p>
+ *
+ * <p><strong>Please do not reuse this code.</strong> Using results in this way requires permission
+ * from Google, and that is not granted to users via this project.</p>
+ *
+ * @author Sean Owen
+ */
 final class ProductResultInfoRetriever extends SupplementalInfoRetriever {
 
   private static final Pattern[] PRODUCT_NAME_PRICE_PATTERNS = {
