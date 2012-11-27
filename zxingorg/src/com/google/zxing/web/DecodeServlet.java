6698e919e99f929c7de7fcce462f88b977f878e4
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2012-11-27 23:57:07 +0000

    More changes for Elastic Beanstalk -- logging, no need for DoS
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2537 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- zxingorg/src/com/google/zxing/web/DecodeServlet.java
+++ zxingorg/src/com/google/zxing/web/DecodeServlet.java
@@ -56,8 +56,6 @@ import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -84,7 +82,6 @@ public final class DecodeServlet extends HttpServlet {
   // No real reason to deal with more than maybe 2 megapixels
   private static final int MAX_PIXELS = 1 << 21;
   private static final byte[] REMAINDER_BUFFER = new byte[8192];
-  private static final long GC_HACK_INTERVAL_MS = 60 * 1000;
   private static final Map<DecodeHintType,Object> HINTS;
   private static final Map<DecodeHintType,Object> HINTS_PURE;
 
@@ -97,19 +94,11 @@ public final class DecodeServlet extends HttpServlet {
   }
 
   private DiskFileItemFactory diskFileItemFactory;
-  private Timer gcHackTimer;
 
   @Override
   public void init(ServletConfig servletConfig) {
     Logger logger = Logger.getLogger("com.google.zxing");
     logger.addHandler(new ServletContextLogHandler(servletConfig.getServletContext()));
-    gcHackTimer = new Timer();
-    gcHackTimer.schedule(new TimerTask() {
-      @Override
-      public void run() {
-        System.gc(); // Hack: GC may close these weird stuck CLOSE_WAIT sockets?
-      }
-    }, GC_HACK_INTERVAL_MS, GC_HACK_INTERVAL_MS);
     diskFileItemFactory = new DiskFileItemFactory();
     log.info("DecodeServlet configured");
   }
@@ -120,7 +109,7 @@ public final class DecodeServlet extends HttpServlet {
 
     String imageURIString = request.getParameter("u");
     if (imageURIString == null || imageURIString.isEmpty()) {
-      log.fine("URI was empty");
+      log.info("URI was empty");
       response.sendRedirect("badurl.jspx");
       return;
     }
@@ -135,15 +124,11 @@ public final class DecodeServlet extends HttpServlet {
     try {
       imageURL = new URI(imageURIString).toURL();
     } catch (URISyntaxException urise) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine("URI was not valid: " + imageURIString);
-      }
+      log.info("URI was not valid: " + imageURIString);
       response.sendRedirect("badurl.jspx");
       return;
     } catch (MalformedURLException mue) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine("URI was not valid: " + imageURIString);
-      }
+      log.info("URI was not valid: " + imageURIString);
       response.sendRedirect("badurl.jspx");
       return;
     }
@@ -152,9 +137,7 @@ public final class DecodeServlet extends HttpServlet {
     try {
       connection = (HttpURLConnection) imageURL.openConnection();
     } catch (IllegalArgumentException iae) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine("URI could not be opened: " + imageURL);
-      }
+      log.info("URI could not be opened: " + imageURL);
       response.sendRedirect("badurl.jspx");
       return;
     }
@@ -175,9 +158,7 @@ public final class DecodeServlet extends HttpServlet {
         //  javax.net.ssl.SSLPeerUnverifiedException,
         //  org.apache.http.NoHttpResponseException,
         //  org.apache.http.client.ClientProtocolException,
-        if (log.isLoggable(Level.FINE)) {
-          log.fine(ioe.toString());
-        }
+        log.info(ioe.toString());
         response.sendRedirect("badurl.jspx");
         return;
       }
@@ -188,14 +169,12 @@ public final class DecodeServlet extends HttpServlet {
         is = connection.getInputStream();
 
         if (connection.getResponseCode() != HttpServletResponse.SC_OK) {
-          if (log.isLoggable(Level.FINE)) {
-            log.fine("Unsuccessful return code: " + connection.getResponseCode());
-          }
+          log.info("Unsuccessful return code: " + connection.getResponseCode());
           response.sendRedirect("badurl.jspx");
           return;
         }
         if (connection.getHeaderFieldInt("Content-Length", 0) > MAX_IMAGE_SIZE) {
-          log.fine("Too large");
+          log.info("Too large");
           response.sendRedirect("badimage.jspx");
           return;
         }
@@ -204,9 +183,7 @@ public final class DecodeServlet extends HttpServlet {
         processStream(is, request, response);
 
       } catch (IOException ioe) {
-        if (log.isLoggable(Level.FINE)) {
-          log.fine(ioe.toString());
-        }
+        log.info(ioe.toString());
         response.sendRedirect("badurl.jspx");
       } finally {
         if (is != null) {
@@ -239,7 +216,7 @@ public final class DecodeServlet extends HttpServlet {
       throws ServletException, IOException {
 
     if (!ServletFileUpload.isMultipartContent(request)) {
-      log.fine("File upload was not multipart");
+      log.info("File upload was not multipart");
       response.sendRedirect("badimage.jspx");
       return;
     }
@@ -260,16 +237,14 @@ public final class DecodeServlet extends HttpServlet {
               is.close();
             }
           } else {
-            log.fine("Too large");
+            log.info("Too large");
             response.sendRedirect("badimage.jspx");
           }
           break;
         }
       }
     } catch (FileUploadException fue) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine(fue.toString());
-      }
+      log.info(fue.toString());
       response.sendRedirect("badimage.jspx");
     }
 
@@ -283,23 +258,17 @@ public final class DecodeServlet extends HttpServlet {
     try {
       image = ImageIO.read(is);
     } catch (IOException ioe) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine(ioe.toString());
-      }
+      log.info(ioe.toString());
       // Includes javax.imageio.IIOException
       response.sendRedirect("badimage.jspx");
       return;
     } catch (CMMException cmme) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine(cmme.toString());
-      }
+      log.info(cmme.toString());
       // Have seen this in logs
       response.sendRedirect("badimage.jspx");
       return;
     } catch (IllegalArgumentException iae) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine(iae.toString());
-      }
+      log.info(iae.toString());
       // Have seen this in logs for some JPEGs
       response.sendRedirect("badimage.jspx");
       return;
@@ -310,9 +279,7 @@ public final class DecodeServlet extends HttpServlet {
     }
     if (image.getHeight() <= 1 || image.getWidth() <= 1 ||
         image.getHeight() * image.getWidth() > MAX_PIXELS) {
-      if (log.isLoggable(Level.FINE)) {
-        log.fine("Dimensions too large: " + image.getWidth() + 'x' + image.getHeight());
-      }
+      log.info("Dimensions too large: " + image.getWidth() + 'x' + image.getHeight());
       response.sendRedirect("badimage.jspx");
       return;
     }
@@ -413,7 +380,6 @@ public final class DecodeServlet extends HttpServlet {
   @Override
   public void destroy() {
     log.config("DecodeServlet shutting down...");
-    gcHackTimer.cancel();
   }
 
 }
