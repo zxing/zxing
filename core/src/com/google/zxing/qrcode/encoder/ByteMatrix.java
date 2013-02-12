62bd9719a884f9b5f7e8a3577c722729fee05779
srowen@gmail.com <srowen@gmail.com@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-02-12 20:08:33 +0000

    Doc an old ByteMatrix method in response to an SO question
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2572 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/qrcode/encoder/ByteMatrix.java
+++ core/src/com/google/zxing/qrcode/encoder/ByteMatrix.java
@@ -17,9 +17,6 @@
 package com.google.zxing.qrcode.encoder;
 
 /**
- * A class which wraps a 2D array of bytes. The default usage is signed. If you want to use it as a
- * unsigned container, it's up to you to do byteValue & 0xff at each location.
- *
  * JAVAPORT: The original code was a 2D array of ints, but since it only ever gets assigned
  * -1, 0, and 1, I'm going to use less memory and go with bytes.
  *
@@ -49,6 +46,9 @@ public final class ByteMatrix {
     return bytes[y][x];
   }
 
+  /**
+   * @return an internal representation as bytes, in row-major order. array[y][x] represents point (x,y)
+   */
   public byte[][] getArray() {
     return bytes;
   }
