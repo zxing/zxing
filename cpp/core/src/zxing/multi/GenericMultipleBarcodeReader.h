8e609df96ee1f646f6cafb8a105eab1ee8c79fb4
smparkes@smparkes.net <smparkes@smparkes.net@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-04-06 20:19:19 +0000

    c++ port of r2585
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2625 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- cpp/core/src/zxing/multi/GenericMultipleBarcodeReader.h
+++ cpp/core/src/zxing/multi/GenericMultipleBarcodeReader.h
@@ -1,3 +1,4 @@
+// -*- mode:c++; tab-width:2; indent-tabs-mode:nil; c-basic-offset:2 -*-
 #ifndef __GENERIC_MULTIPLE_BARCODE_READER_H__
 #define __GENERIC_MULTIPLE_BARCODE_READER_H__
 
@@ -22,26 +23,29 @@
 
 namespace zxing {
 namespace multi {
-class GenericMultipleBarcodeReader : public MultipleBarcodeReader {
-  private:
-    static Ref<Result> translateResultPoints(Ref<Result> result, 
-                                             int xOffset, 
-                                             int yOffset);
-    void doDecodeMultiple(Ref<BinaryBitmap> image, 
-                          DecodeHints hints, 
-                          std::vector<Ref<Result> >& results, 
-                          int xOffset, 
-                          int yOffset);             
-    Reader& delegate_;
-    static const int MIN_DIMENSION_TO_RECUR = 100;
 
-  public:
-    GenericMultipleBarcodeReader(Reader& delegate);
-    virtual ~GenericMultipleBarcodeReader();
-    virtual std::vector<Ref<Result> > decodeMultiple(Ref<BinaryBitmap> image, 
-                                                     DecodeHints hints);
+class GenericMultipleBarcodeReader : public MultipleBarcodeReader {
+ private:
+  static Ref<Result> translateResultPoints(Ref<Result> result, 
+                                           int xOffset, 
+                                           int yOffset);
+  void doDecodeMultiple(Ref<BinaryBitmap> image, 
+                        DecodeHints hints, 
+                        std::vector<Ref<Result> >& results, 
+                        int xOffset, 
+                        int yOffset,
+                        int currentDepth);
+  Reader& delegate_;
+  static const int MIN_DIMENSION_TO_RECUR = 100;
+  static const int MAX_DEPTH = 4;
+
+ public:
+  GenericMultipleBarcodeReader(Reader& delegate);
+  virtual ~GenericMultipleBarcodeReader();
+  virtual std::vector<Ref<Result> > decodeMultiple(Ref<BinaryBitmap> image, DecodeHints hints);
 };
-} // End zxing::multi namespace
-} // End zxing namespace
+
+}
+}
 
 #endif // __GENERIC_MULTIPLE_BARCODE_READER_H__
