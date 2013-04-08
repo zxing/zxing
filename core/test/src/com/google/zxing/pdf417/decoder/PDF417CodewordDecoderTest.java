5117d7118453189eaf5df0bacba637266702993b
Guenther Grau <zxing.org@grau.net>
2013-04-08 22:19:49 +0200

    add more test codewords

--- core/test/src/com/google/zxing/pdf417/decoder/PDF417CodewordDecoderTest.java
+++ core/test/src/com/google/zxing/pdf417/decoder/PDF417CodewordDecoderTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 public class PDF417CodewordDecoderTest {
 
   private static final int[][] inputValid = {
+      { 21, 3, 6, 6, 5, 10, 10, 6 },
+      { 22, 3, 5, 14, 5, 3, 9, 6 },
       { 17, 3, 5, 3, 9, 3, 8, 18 },
       { 2, 2, 3, 1, 4, 6, 8, 2 },
       { 2, 6, 4, 4, 4, 3, 2, 3 },
@@ -30,6 +32,8 @@ public class PDF417CodewordDecoderTest {
   };
 
   private static final int[][] expectedValid = {
+      { 5, 1, 1, 2, 1, 3, 2, 2 },
+      { 5, 1, 1, 4, 1, 1, 2, 2 },
       { 4, 1, 1, 1, 2, 1, 2, 5 },
       { 1, 2, 1, 1, 2, 4, 5, 1 },
       { 1, 4, 2, 3, 2, 2, 1, 2 },
@@ -53,6 +57,7 @@ public class PDF417CodewordDecoderTest {
   };
 
   private static final int[][] inputInvalid = {
+      { 21, 4, 6, 6, 6, 9, 10, 5 },
       { 3, 1, 2, 8, 1, 2, 2, 6 },
       { 2, 3, 7, 5, 2, 4, 1, 2 },
   };
@@ -72,12 +77,9 @@ public class PDF417CodewordDecoderTest {
     }
   }
 
-  //{2,1,1,5,1,2,1,4}
-  //{2,1,1,6,1,1,1,4}
-
   @Test
   public void testValues() {
-    System.out.println(BitMatrixParser.getCodeword(getDecodedValue(new int[] { 2, 1, 1, 5, 1, 2, 1, 4 })));
+    System.out.println(BitMatrixParser.getCodeword(getDecodedValue(new int[] { 5, 1, 1, 2, 1, 3, 2, 2 })));
   }
 
   @Test
