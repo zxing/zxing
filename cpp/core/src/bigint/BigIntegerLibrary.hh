2c2bff85b3d67156e57db2b5695b6dd6f2f74ebc
smparkes@smparkes.net <smparkes@smparkes.net@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-04-21 02:47:09 +0000

    import https://mattmccutchen.net/bigint/
    git-svn-id: http://zxing.googlecode.com/svn/trunk@2662 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ cpp/core/src/bigint/BigIntegerLibrary.hh
@@ -0,0 +1,8 @@
+// This header file includes all of the library header files.
+
+#include "NumberlikeArray.hh"
+#include "BigUnsigned.hh"
+#include "BigInteger.hh"
+#include "BigIntegerAlgorithms.hh"
+#include "BigUnsignedInABase.hh"
+#include "BigIntegerUtils.hh"
