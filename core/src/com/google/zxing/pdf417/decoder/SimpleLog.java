b57d13ab9395e919f4a8d95062652637c1bbfd65
Guenther Grau <zxing.org@grau.net>
2013-04-26 18:36:32 +0200

    debug to devel

--- core/src/com/google/zxing/pdf417/decoder/SimpleLog.java
+++ core/src/com/google/zxing/pdf417/decoder/SimpleLog.java
@@ -19,7 +19,7 @@ public class SimpleLog {
     FATAL
   }
 
-  protected static LEVEL LOG_LEVEL = LEVEL.ERROR;
+  protected static LEVEL LOG_LEVEL = LEVEL.DEVEL;
 
   public static void log(LEVEL level, String string, int imageRow, int imageColumn, int barcodeRow, int barcodeColumn) {
     log(level, string + ", barcodeRow: " + barcodeRow + ", barcodeColumn: " + barcodeColumn, imageRow, imageColumn);
