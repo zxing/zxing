ea1c78af59e00468c339dcbce4bf41a286219896
srowen@gmail.com <srowen@gmail.com@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-06-18 16:58:29 +0000

    Issue 1712 fix test case too
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2821 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/test/src/com/google/zxing/oned/CodaBarWriterTestCase.java
+++ core/test/src/com/google/zxing/oned/CodaBarWriterTestCase.java
@@ -34,7 +34,7 @@ public final class CodaBarWriterTestCase extends Assert {
     String resultStr = "0000000000" +
         "1001001011011010100101010110010110101001010100110101100101010110110101101001001011"
         + "0000000000";
-    BitMatrix result = new CodaBarWriter().encode("B515-3/N", BarcodeFormat.CODABAR, resultStr.length(), 0);
+    BitMatrix result = new CodaBarWriter().encode("B515-3/B", BarcodeFormat.CODABAR, resultStr.length(), 0);
     for (int i = 0; i < resultStr.length(); i++) {
       assertEquals("Element " + i, resultStr.charAt(i) == '1', result.get(i, 0));
     }
