8e1b1a3bb424307c1a857c117870a5f78509e7e6
srowen@gmail.com <srowen@gmail.com@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-07-01 19:29:10 +0000

    Fix handling of book search snippets without page number
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2835 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/book/SearchBookContentsActivity.java
+++ android/src/com/google/zxing/client/android/book/SearchBookContentsActivity.java
@@ -239,38 +239,43 @@ public final class SearchBookContentsActivity extends Activity {
       }
     }
 
-    // Available fields: page_id, page_number, page_url, snippet_text
+    // Available fields: page_id, page_number, snippet_text
     private SearchBookContentsResult parseResult(JSONObject json) {
-      try {
-        String pageId = json.getString("page_id");
-        String pageNumber = json.getString("page_number");
-        if (pageNumber.isEmpty()) {
-          // This can happen for text on the jacket, and possibly other reasons.
-          pageNumber = getString(R.string.msg_sbc_unknown_page);
-        } else {
-          pageNumber = getString(R.string.msg_sbc_page) + ' ' + pageNumber;
-        }
 
-        // Remove all HTML tags and encoded characters. Ideally the server would do this.
-        String snippet = json.optString("snippet_text");
-        boolean valid = true;
-        if (snippet.isEmpty()) {
-          snippet = '(' + getString(R.string.msg_sbc_snippet_unavailable) + ')';
-          valid = false;
-        } else {
-          snippet = TAG_PATTERN.matcher(snippet).replaceAll("");
-          snippet = LT_ENTITY_PATTERN.matcher(snippet).replaceAll("<");
-          snippet = GT_ENTITY_PATTERN.matcher(snippet).replaceAll(">");
-          snippet = QUOTE_ENTITY_PATTERN.matcher(snippet).replaceAll("'");
-          snippet = QUOT_ENTITY_PATTERN.matcher(snippet).replaceAll("\"");
-        }
-        return new SearchBookContentsResult(pageId, pageNumber, snippet, valid);
+      String pageId;
+      String pageNumber;
+      String snippet;
+      try {
+        pageId = json.getString("page_id");
+        pageNumber = json.optString("page_number");
+        snippet = json.optString("snippet_text");        
       } catch (JSONException e) {
+        Log.w(TAG, e);
         // Never seen in the wild, just being complete.
         return new SearchBookContentsResult(getString(R.string.msg_sbc_no_page_returned), "", "", false);
       }
-    }
+      
+      if (pageNumber == null || pageNumber.isEmpty()) {
+        // This can happen for text on the jacket, and possibly other reasons.
+        pageNumber = "";
+      } else {
+        pageNumber = getString(R.string.msg_sbc_page) + ' ' + pageNumber;
+      }
+      
+      boolean valid = snippet != null && !snippet.isEmpty();
+      if (valid) {
+        // Remove all HTML tags and encoded characters.          
+        snippet = TAG_PATTERN.matcher(snippet).replaceAll("");
+        snippet = LT_ENTITY_PATTERN.matcher(snippet).replaceAll("<");
+        snippet = GT_ENTITY_PATTERN.matcher(snippet).replaceAll(">");
+        snippet = QUOTE_ENTITY_PATTERN.matcher(snippet).replaceAll("'");
+        snippet = QUOT_ENTITY_PATTERN.matcher(snippet).replaceAll("\"");
+      } else {
+        snippet = '(' + getString(R.string.msg_sbc_snippet_unavailable) + ')';        
+      }
 
+      return new SearchBookContentsResult(pageId, pageNumber, snippet, valid);
+    }
 
   }
 
