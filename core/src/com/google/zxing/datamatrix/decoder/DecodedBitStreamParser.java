2935184135890f5c350e19e2b05d268014907d4d
srowen@gmail.com <srowen@gmail.com@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-07-28 09:49:23 +0000

    Issue 1742 fix back-tick in text encodation set
    git-svn-id: https://zxing.googlecode.com/svn/trunk@2850 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- core/src/com/google/zxing/datamatrix/decoder/DecodedBitStreamParser.java
+++ core/src/com/google/zxing/datamatrix/decoder/DecodedBitStreamParser.java
@@ -72,7 +72,7 @@ final class DecodedBitStreamParser {
   };
 
   private static final char[] TEXT_SHIFT3_SET_CHARS = {
-    '\'', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    '`', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
     'O',  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '{', '|', '}', '~', (char) 127
   };
 
