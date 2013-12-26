e17d73cc9e866065e7a8824d5cb52ee6ff639d41
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2013-12-26 19:25:19 +0000

    Check in Android gen files and configure Android src roots in Maven
    git-svn-id: https://zxing.googlecode.com/svn/trunk@3004 59b500cc-1b3d-0410-9834-0bbf25fbcc57

---
+++ androidtest/gen/R.java.d
@@ -0,0 +1,10 @@
+/Users/srowen/Documents/ZXing/androidtest/gen/com/google/zxing/client/androidtest/R.java \
+ : /Users/srowen/Documents/ZXing/androidtest/res/drawable/icon.png \
+/Users/srowen/Documents/ZXing/androidtest/res/drawable-hdpi/icon.png \
+/Users/srowen/Documents/ZXing/androidtest/res/drawable-xhdpi/icon.png \
+/Users/srowen/Documents/ZXing/androidtest/res/layout/benchmark.xml \
+/Users/srowen/Documents/ZXing/androidtest/res/layout/test.xml \
+/Users/srowen/Documents/ZXing/androidtest/res/menu/main.xml \
+/Users/srowen/Documents/ZXing/androidtest/res/values/ids.xml \
+/Users/srowen/Documents/ZXing/androidtest/res/values/strings.xml \
+/Users/srowen/Documents/ZXing/androidtest/bin/AndroidManifest.xml \
---
+++ androidtest/gen/com/google/zxing/client/androidtest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.zxing.client.androidtest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
