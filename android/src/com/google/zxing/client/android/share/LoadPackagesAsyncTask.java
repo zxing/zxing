caa4ee02d55c6a6eebab0dfd264331adcc73145b
srowen <srowen@59b500cc-1b3d-0410-9834-0bbf25fbcc57>
2014-01-01 13:29:43 +0000

    Fix problem on some platforms with AsyncTask array arg type
    git-svn-id: https://zxing.googlecode.com/svn/trunk@3007 59b500cc-1b3d-0410-9834-0bbf25fbcc57

--- android/src/com/google/zxing/client/android/share/LoadPackagesAsyncTask.java
+++ android/src/com/google/zxing/client/android/share/LoadPackagesAsyncTask.java
@@ -38,7 +38,7 @@ import java.util.List;
  *
  * @author Sean Owen
  */
-final class LoadPackagesAsyncTask extends AsyncTask<Void,Void,List<AppInfo>> {
+final class LoadPackagesAsyncTask extends AsyncTask<Object,Object,List<AppInfo>> {
 
   private static final String[] PKG_PREFIX_WHITELIST = {
       "com.google.android.apps.",
@@ -57,7 +57,7 @@ final class LoadPackagesAsyncTask extends AsyncTask<Void,Void,List<AppInfo>> {
   }
 
   @Override
-  protected List<AppInfo> doInBackground(Void... objects) {
+  protected List<AppInfo> doInBackground(Object... objects) {
     List<AppInfo> labelsPackages = new ArrayList<>();
     PackageManager packageManager = activity.getPackageManager();
     Iterable<ApplicationInfo> appInfos = packageManager.getInstalledApplications(0);
