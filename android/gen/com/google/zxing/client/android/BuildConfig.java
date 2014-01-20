59e4708cd7a894722d3eaf02c1be8d709c40bad4
Sean Owen <srowen@gmail.com>
2014-01-20 17:21:09 +0000

    Fix up .gitignore to ignore generated dirs, and remove generated dirs from git

--- android/gen/com/google/zxing/client/android/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.zxing.client.android;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- androidtest/gen/com/google/zxing/client/androidtest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.zxing.client.androidtest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
