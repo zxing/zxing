e45d17582963ae8f2b5a41291c4efbbaa43d5bac
mkicinski <Michael.Kicinski@colinx.com>
2014-02-17 15:50:18 -0500

    Unit test FNC1 and FNC2

--- core/src/test/java/com/google/zxing/oned/Code128WriterTestCase.java
+++ core/src/test/java/com/google/zxing/oned/Code128WriterTestCase.java
@@ -1,5 +1,8 @@
 package com.google.zxing.oned;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.zxing.BarcodeFormat;
@@ -7,13 +10,21 @@ import com.google.zxing.Writer;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 
-import static org.junit.Assert.*;
-
 public class Code128WriterTestCase {
+	private static final String FNC1 = "11110101110";
+	private static final String FNC2 = "11110101000";
 	private static final String FNC3 = "10111100010";
+	private static final String FNC4 = "10111101110";
 	private static final String START_CODE_B = "11010010000";
 	public static final String QUIET_SPACE = "00000";
 	public static final String STOP = "1100011101011";
+	
+	Writer writer;
+	
+	@Before
+	public void setup() {
+		writer = new Code128Writer();
+	}
 	
 	@Test
 	public void testEncodeWithFunc3() throws WriterException {
@@ -21,13 +32,53 @@ public class Code128WriterTestCase {
 		//                                                       "1"            "2"             "3"          check digit 51
 		String expected = QUIET_SPACE + START_CODE_B + FNC3 + "10011100110" + "11001110010" + "11001011100" + "11101000110" + STOP + QUIET_SPACE;
 		
-		Writer writer = new Code128Writer();
 		BitMatrix result = writer.encode(toEncode, BarcodeFormat.CODE_128, 0, 0);
 		
-		StringBuilder actual = new StringBuilder(result.getWidth());
+		String actual = matrixToString(result);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testEncodeWithFunc2() throws WriterException {
+		String toEncode = "\u00f2" + "123";
+		//                                                       "1"            "2"             "3"          check digit 56
+		String expected = QUIET_SPACE + START_CODE_B + FNC2 + "10011100110" + "11001110010" + "11001011100" + "11100010110" + STOP + QUIET_SPACE;
+		
+		BitMatrix result = writer.encode(toEncode, BarcodeFormat.CODE_128, 0, 0);
+		
+		String actual = matrixToString(result);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testEncodeWithFunc1() throws WriterException {
+		String toEncode = "\u00f1" + "123";
+		//                                                       "1"            "2"             "3"          check digit 61
+		String expected = QUIET_SPACE + START_CODE_B + FNC1 + "10011100110" + "11001110010" + "11001011100" + "11001000010" + STOP + QUIET_SPACE;
+		
+		BitMatrix result = writer.encode(toEncode, BarcodeFormat.CODE_128, 0, 0);
+		
+		String actual = matrixToString(result);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testEncodeWithFunc4() throws WriterException {
+		String toEncode = "\u00f4" + "123";
+		//                                                       "1"            "2"             "3"          check digit 59
+		String expected = QUIET_SPACE + START_CODE_B + FNC4 + "10011100110" + "11001110010" + "11001011100" + "11100011010" + STOP + QUIET_SPACE;
+		
+		BitMatrix result = writer.encode(toEncode, BarcodeFormat.CODE_128, 0, 0);
+		
+		String actual = matrixToString(result);
+		assertEquals(expected, actual);
+	}
+
+	private String matrixToString(BitMatrix result) {
+		StringBuilder builder = new StringBuilder(result.getWidth());
 		for (int i = 0; i < result.getWidth(); i++) {
-			actual.append(result.get(i, 0) ? '1' : '0');
+			builder.append(result.get(i, 0) ? '1' : '0');
 		}
-		assertEquals(expected, actual.toString());
+		return builder.toString();
 	}
 }
