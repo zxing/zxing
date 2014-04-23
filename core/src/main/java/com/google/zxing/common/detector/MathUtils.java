3b8b44856d050520b99035777d5dcc73a8e56edd
Sean Owen <srowen@gmail.com>
2014-04-23 22:18:54 +0100

    For safety, make optimized round method consistent with Math.round for negative input too

--- core/src/main/java/com/google/zxing/common/detector/MathUtils.java
+++ core/src/main/java/com/google/zxing/common/detector/MathUtils.java
@@ -23,13 +23,15 @@ public final class MathUtils {
 
   /**
    * Ends up being a bit faster than {@link Math#round(float)}. This merely rounds its
-   * argument to the nearest int, where x.5 rounds up to x+1.
+   * argument to the nearest int, where x.5 rounds up to x+1. Semantics of this shortcut
+   * differ slightly from {@link Math#round(float)} in that half rounds down for negative
+   * values. -2.5 rounds to -3, not -2. For purposes here it makes no difference.
    *
    * @param d real value to round
    * @return nearest {@code int}
    */
   public static int round(float d) {
-    return (int) (d + 0.5f);
+    return (int) (d + (d < 0.0f ? -0.5f : 0.5f));
   }
 
   public static float distance(float aX, float aY, float bX, float bY) {
