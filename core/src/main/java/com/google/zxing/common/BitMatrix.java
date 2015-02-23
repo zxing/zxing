8475bf6fd24f021d596a1a134df34a0b7c0b6c55
Sean Owen <srowen@gmail.com>
2015-02-23 16:08:36 +0000

    Remove system-dependent line separator logic in BitMatrix

--- core/src/main/java/com/google/zxing/common/BitMatrix.java
+++ core/src/main/java/com/google/zxing/common/BitMatrix.java
@@ -414,9 +414,13 @@ public final class BitMatrix implements Cloneable {
   }
 
   public String toString(String setString, String unsetString) {
-    return toString(setString, unsetString, System.lineSeparator());
+    return toString(setString, unsetString, "\n");
   }
-  
+
+  /**
+   * @deprecated call {@link #toString(String,String)} only, which uses \n line separator always
+   */
+  @Deprecated
   public String toString(String setString, String unsetString, String lineSeparator) {
     StringBuilder result = new StringBuilder(height * (width + 1));
     for (int y = 0; y < height; y++) {
