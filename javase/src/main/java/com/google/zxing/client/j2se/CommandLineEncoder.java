061d68e528224d1289d591e0f5ee5d1a54a7bb19
Loikki <loic_hausammann@hotmail.com>
2015-09-22 09:02:27 +0200

    add option to choose the correction level in the encoder

--- javase/src/main/java/com/google/zxing/client/j2se/CommandLineEncoder.java
+++ javase/src/main/java/com/google/zxing/client/j2se/CommandLineEncoder.java
@@ -19,9 +19,12 @@ package com.google.zxing.client.j2se;
 import com.beust.jcommander.JCommander;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * Command line utility for encoding barcodes.
@@ -41,15 +44,31 @@ public final class CommandLineEncoder {
       jCommander.usage();
       return;
     }
-
     String outFileString = config.outputFileBase;
     if (EncoderConfig.DEFAULT_OUTPUT_FILE_BASE.equals(outFileString)) {
       outFileString += '.' + config.imageFormat.toLowerCase(Locale.ENGLISH);
     }
     
-    BitMatrix matrix = new MultiFormatWriter().encode(
-        config.contents.get(0), config.barcodeFormat, config.width, config.height);
-    MatrixToImageWriter.writeToPath(matrix, config.imageFormat, Paths.get(outFileString));
+    if (config.barcodeFormat == BarcodeFormat.QR_CODE) {
+        Map<EncodeHintType,?> hints = new Map<EncodeHintType,?>;
+        if (config.errorcorrection == 'H') {
+            hints.put(EncodeHintType.ERRORCORRECTION,ErrorCorrectionLevel.H);
+        } else if (config.errorcorrection == 'L') {
+            hints.put(EncodeHintType.ERRORCORRECTION,ErrorCorrectionLevel.L);
+        } else if (config.errorcorrection == 'Q') {
+            hints.put(EncodeHintType.ERRORCORRECTION,ErrorCorrectionLevel.Q);
+        } else {
+            hints.put(EncodeHintType.ERRORCORRECTION,ErrorCorrectionLevel.M);
+        }
+        BitMatrix matrix = new MultiFormatWriter().encode(config.contents.get(0), config.barcodeFormat,
+                                                          config.width, config.height, hints);
+        MatrixToImageWriter.writeToPath(matrix, config.imageFormat, Paths.get(outFileString));
+        
+    } else {
+        BitMatrix matrix = new MultiFormatWriter().encode(config.contents.get(0), config.barcodeFormat,
+                                                          config.width, config.height);
+        MatrixToImageWriter.writeToPath(matrix, config.imageFormat, Paths.get(outFileString));
+    }
   }
 
 }
