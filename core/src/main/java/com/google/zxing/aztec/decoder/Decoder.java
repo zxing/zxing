e3d58752ee20386ec45ca0e54d9562cc0d793415
Arno Richter <oelna@oelna.de>
2016-04-24 13:56:06 +0200

    add missing braces for if and for statements per @srowen's request.

--- core/src/main/java/com/google/zxing/aztec/decoder/Decoder.java
+++ core/src/main/java/com/google/zxing/aztec/decoder/Decoder.java
@@ -336,16 +336,18 @@ public final class Decoder {
   private static byte boolsToByte(final boolean[] array, final int start) {
     byte b = 0;
     for (int i = 0; i < 8; i++) {
-      if (array[start + i])
+      if (array[start + i]) {
         b |= 1 << (7 - i);
+      }
     }
     return b;
   }
 
   public static byte[] convertBoolArrayToByteArray(boolean[] boolArr) {
     byte[] byteArr = new byte[boolArr.length / 8];
-    for (int i = 0; i < byteArr.length; i++)
+    for (int i = 0; i < byteArr.length; i++) {
       byteArr[i] = boolsToByte(boolArr, 8 * i);
+    }
     return byteArr;
   }
 
