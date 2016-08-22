7805a7551ced99adbcab5e8f272e14247f95c186
Sebastian Buchwald <UniQP@users.noreply.github.com>
2016-08-22 11:29:31 +0200

    Fix some typos (#651)

--- core/src/main/java/com/google/zxing/datamatrix/decoder/Version.java
+++ core/src/main/java/com/google/zxing/datamatrix/decoder/Version.java
@@ -139,7 +139,7 @@ public final class Version {
   }
 
   /**
-   * <p>Encapsualtes the parameters for one error-correction block in one symbol version.
+   * <p>Encapsulates the parameters for one error-correction block in one symbol version.
    * This includes the number of data codewords, and the number of times a block with these
    * parameters is used consecutively in the Data Matrix code version's format.</p>
    */
