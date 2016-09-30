75c51f93feb66a96b6604af4804053cbbfb8d2ea
Sean Owen <srowen@gmail.com>
2016-09-30 17:41:01 -0500

    Update lax count for qr code 2

--- core/src/test/java/com/google/zxing/qrcode/QRCodeBlackBox2TestCase.java
+++ core/src/test/java/com/google/zxing/qrcode/QRCodeBlackBox2TestCase.java
@@ -27,7 +27,7 @@ public final class QRCodeBlackBox2TestCase extends AbstractBlackBoxTestCase {
 
   public QRCodeBlackBox2TestCase() {
     super("src/test/resources/blackbox/qrcode-2", new MultiFormatReader(), BarcodeFormat.QR_CODE);
-    addTest(30, 30, 0.0f);
+    addTest(31, 31, 0.0f);
     addTest(29, 29, 90.0f);
     addTest(30, 30, 180.0f);
     addTest(29, 29, 270.0f);
