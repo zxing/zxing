96641c620940273bda33d74e9eb7adbd00398673
David L. Dawes <davidlyman.dawes@capitalone.com>
2016-12-17 20:48:14 -0800

    Fiddling with getting modifications into place.

---
+++ android/app/build/generated/source/buildConfig/androidTest/debug/com/google/zxing/client/android/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.zxing.client.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.zxing.client.android.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
---
+++ android/app/src/main/java/com/google/zxing/client/android/LicenceActivity.java
@@ -0,0 +1,13 @@
+package com.google.zxing.client.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LicenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_licence);
+    }
+}
