18fa13b6f7ea944ccba87e231143ca80ee982448
Bhargav M S <bhargav521@gmail.com>
2016-12-20 00:40:25 +0530

    Do open camera in a background thread

--- android/src/com/google/zxing/client/android/camera/CameraManager.java
+++ android/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -67,6 +67,50 @@ public final class CameraManager {
     this.configManager = new CameraConfigurationManager(context);
     previewCallback = new PreviewCallback(configManager);
   }
+
+  synchronized void openDriver() throws IOException {
+    OpenCamera theCamera = camera;
+    if (theCamera == null) {
+      theCamera = OpenCameraInterface.open(requestedCameraId);
+      if (theCamera == null) {
+        throw new IOException("Camera.open() failed to return object from driver");
+      }
+      camera = theCamera;
+    }
+
+    if (!initialized) {
+      initialized = true;
+      configManager.initFromCameraParameters(theCamera);
+      if (requestedFramingRectWidth > 0 && requestedFramingRectHeight > 0) {
+        setManualFramingRect(requestedFramingRectWidth, requestedFramingRectHeight);
+        requestedFramingRectWidth = 0;
+        requestedFramingRectHeight = 0;
+      }
+    }
+
+    Camera cameraObject = theCamera.getCamera();
+    Camera.Parameters parameters = cameraObject.getParameters();
+    String parametersFlattened = parameters == null ? null : parameters.flatten(); // Save these, temporarily
+    try {
+      configManager.setDesiredCameraParameters(theCamera, false);
+    } catch (RuntimeException re) {
+      // Driver failed
+      Log.w(TAG, "Camera rejected parameters. Setting only minimal safe-mode parameters");
+      Log.i(TAG, "Resetting to saved camera params: " + parametersFlattened);
+      // Reset:
+      if (parametersFlattened != null) {
+        parameters = cameraObject.getParameters();
+        parameters.unflatten(parametersFlattened);
+        try {
+          cameraObject.setParameters(parameters);
+          configManager.setDesiredCameraParameters(theCamera, true);
+        } catch (RuntimeException re2) {
+          // Well, darn. Give up
+          Log.w(TAG, "Camera rejected even safe-mode parameters! No configuration");
+        }
+      }
+    }
+  }
   
   /**
    * Opens the camera driver and initializes the hardware parameters.
@@ -329,4 +373,69 @@ public final class CameraManager {
                                         rect.width(), rect.height(), false);
   }
 
+
+  private static class CameraHandlerThread extends HandlerThread {
+    Handler mHandler = null;
+    private Handler mainThreadHandler;
+    private WeakReference<CameraManager> cameraManagerWeakReference;
+
+    CameraHandlerThread(CameraManager manager) {
+      super("CameraHandlerThread");
+      start();
+      mHandler = new Handler(getLooper());
+      mainThreadHandler = new Handler(Looper.getMainLooper());
+      cameraManagerWeakReference = new WeakReference<>(manager);
+    }
+
+    public synchronized void openDriver(CameraInitListener initListener, SurfaceHolder holder) {
+      new CameraHandlerThread(this).openDriver(initListener, holder);
+    }
+
+    void openDriver(final CameraInitListener listener, SurfaceHolder surfaceHolder) {
+      final WeakReference<SurfaceHolder> mH = new WeakReference<>(surfaceHolder);
+      mHandler.post(new Runnable() {
+        @Override
+        public void run() {
+          final CameraManager manager = cameraManagerWeakReference.get();
+          if (manager != null) {
+            try {
+              listener.onStartInit();
+              manager.openDriver();
+            } catch (IOException e) {
+              mainThreadHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                  listener.onError();
+                }
+              });
+              return;
+            }
+            mainThreadHandler.post(new Runnable() {
+              @Override
+              public void run() {
+                SurfaceHolder sh = mH.get();
+                if (sh != null && manager.camera != null) {
+                  try {
+                    manager.camera.getCamera().setPreviewDisplay(sh);
+                    listener.onInit();
+                  } catch (IOException e) {
+                    listener.onError();
+                    e.printStackTrace();
+                  }
+                }
+              }
+            });
+          }
+        }
+      });
+    }
+  }
+
+  public interface CameraInitListener {
+    void onStartInit();
+
+    void onInit();
+
+    void onError();
+  }
 }
