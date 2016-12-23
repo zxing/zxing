327d05f106d798db6d4838c8acc2be379a331b3f
David L. Dawes <davidlyman.dawes@capitalone.com>
2016-12-23 01:33:46 -0800

    Get ads in, next up: hook up the scanning.

--- android/app/src/main/java/com/kochzap/StartActivity.java
+++ android/app/src/main/java/com/kochzap/StartActivity.java
@@ -1,12 +1,16 @@
-    package com.kochzap;
+package com.kochzap;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import com.kochzap.history.HistoryActivity;
 import com.kochzap.share.ShareActivity;
@@ -23,7 +27,7 @@ public class StartActivity extends AppCompatActivity {
         String company = myIntent.getStringExtra("company");
         String scan = myIntent.getStringExtra("scan");
 
-
+        showAd();
     }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -69,5 +73,35 @@ public class StartActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    private class AdTrack {
+
+        public Activity ctx;
+        public AdView adView, adView1, adView2;
+        public AdRequest adRequest, adRequest1, adRequest2;
+
+        AdTrack(Activity context, AdView adVw, AdRequest adReq) {
+            this.ctx = context;
+            this.adView = adVw;
+            this.adRequest = adReq;
+        }
+    }
+
+    private AdTrack adTrack, adTrack1, adTrack2;
+
+    private void showAd() {
+        if (adTrack == null || adTrack.ctx != this) {
+            // need a new ad
+            AdView mAdView = (AdView) findViewById(R.id.BoycottAdView);
+            AdRequest adRequest = new AdRequest.Builder()
+                    .build();
+            mAdView.loadAd(adRequest);
+            // OK, load this ad for this context
+            adTrack = new AdTrack(this, mAdView, adRequest);
+        } else {
+            // already fetched the ad for this context, just load it again
+            adTrack.adView.loadAd(adTrack.adRequest);
+        }
     }
 }
