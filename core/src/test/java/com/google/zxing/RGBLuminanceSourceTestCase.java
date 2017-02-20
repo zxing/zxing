40b34bdd6c080f8d2d6f9e5f5566d91214c60521
David Sanders <dsanders11@ucsbalum.com>
2017-02-20 05:35:33 -0800

    Add new matrix tests in RGBLuminanceSourceTestCase

--- core/src/test/java/com/google/zxing/RGBLuminanceSourceTestCase.java
+++ core/src/test/java/com/google/zxing/RGBLuminanceSourceTestCase.java
@@ -39,6 +39,12 @@ public final class RGBLuminanceSourceTestCase extends Assert {
   public void testMatrix() {
     assertArrayEquals(new byte[] { 0x00, 0x7F, (byte) 0xFF, 0x3F, 0x7F, 0x3F, 0x3F, 0x7F, 0x3F },
                       SOURCE.getMatrix());
+    LuminanceSource croppedFullWidth = SOURCE.crop(0, 1, 3, 2);
+    assertArrayEquals(new byte[] { 0x3F, 0x7F, 0x3F, 0x3F, 0x7F, 0x3F },
+                      croppedFullWidth.getMatrix());
+    LuminanceSource croppedCorner = SOURCE.crop(1, 1, 2, 2);
+    assertArrayEquals(new byte[] { 0x7F, 0x3F, 0x7F, 0x3F },
+                      croppedCorner.getMatrix());
   }
 
   @Test
