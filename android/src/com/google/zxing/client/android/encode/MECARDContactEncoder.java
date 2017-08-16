cbdf08793329abc336dde0e52099f5c5e9fbab87
Sean Owen <srowen@gmail.com>
2017-08-16 12:29:57 +0100

    Issue #842: allow + in MECARD TEL lines

--- android/src/com/google/zxing/client/android/encode/MECARDContactEncoder.java
+++ android/src/com/google/zxing/client/android/encode/MECARDContactEncoder.java
@@ -79,10 +79,10 @@ final class MECARDContactEncoder extends ContactEncoder {
   }
 
   private static class MECARDTelDisplayFormatter implements Formatter {
-    private static final Pattern NOT_DIGITS = Pattern.compile("[^0-9]+");
+    private static final Pattern NOT_DIGITS_OR_PLUS = Pattern.compile("[^0-9+]+");
     @Override
     public CharSequence format(CharSequence value, int index) {
-      return NOT_DIGITS.matcher(PhoneNumberUtils.formatNumber(value.toString())).replaceAll("");
+      return NOT_DIGITS_OR_PLUS.matcher(PhoneNumberUtils.formatNumber(value.toString())).replaceAll("");
     }
   }
 
