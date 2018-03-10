dba4ef3259dbc8e610e20085eb0360efa7687eeb
LNDDYL <LNDDYL@users.noreply.github.com>
2018-03-10 12:51:42 +0800

    Fix

--- android/src/com/google/zxing/client/android/LocaleManager.java
+++ android/src/com/google/zxing/client/android/LocaleManager.java
@@ -105,7 +105,7 @@ public final class LocaleManager {
   private static final Map<String,String> GOOGLE_BOOK_SEARCH_COUNTRY_TLD = GOOGLE_COUNTRY_TLD;
 
   private static final Collection<String> TRANSLATED_HELP_ASSET_LANGUAGES =
-      Arrays.asList("de", "en", "es", "fa", "fr", "it", "ja", "ko", "nl", "pt", "ru", "uk", "zh-rCN", "zh-rTW", "zh-rHK",, "zh-rSG");
+      Arrays.asList("de", "en", "es", "fa", "fr", "it", "ja", "ko", "nl", "pt", "ru", "uk", "zh-rCN", "zh-rTW", "zh-rHK", "zh-rSG");
 
   private LocaleManager() {}
 
