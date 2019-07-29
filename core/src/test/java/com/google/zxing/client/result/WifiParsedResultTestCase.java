4d94ac22eb8cc7b0b5ec8176aab6b40db27e8db5
Sean Owen <srowen@gmail.com>
2019-07-29 14:29:24 -0500

    Add addition wifi: QR code test case from mailing list

--- core/src/test/java/com/google/zxing/client/result/WifiParsedResultTestCase.java
+++ core/src/test/java/com/google/zxing/client/result/WifiParsedResultTestCase.java
@@ -67,6 +67,7 @@ public final class WifiParsedResultTestCase extends Assert {
   @Test
   public void testEscape() {
     doTest("WIFI:T:WPA;S:test;P:my_password\\\\;;", "test", "my_password\\", "WPA");
+    doTest("WIFI:T:WPA;S:My_WiFi_SSID;P:abc123/;;", "My_WiFi_SSID", "abc123/", "WPA");
   }
 
   /**
