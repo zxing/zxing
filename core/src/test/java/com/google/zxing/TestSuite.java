0cfac15e7fcd64db87894464483156db89234ce8
fleppe <fmagnell@kth.se>
2020-02-13 14:58:47 +0100

    [ADD] - added test suite

---
+++ core/src/test/java/com/google/zxing/TestSuite.java
@@ -0,0 +1,31 @@
+package com.google.zxing;
+
+import com.google.zxing.oned.Code39ExtendedBlackBox2TestCase;
+import com.google.zxing.oned.Code39ExtendedModeTestCase;
+import junit.framework.TestCase;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Code39ExtendedModeTestCase.class, Code39ExtendedBlackBox2TestCase.class})
+public class TestSuite extends TestCase {
+  /**
+   * One time setup before the tests are run.
+   */
+  @BeforeClass
+  public static void setup() {
+    CoverageTool2000.initCoverageMatrix(0, 24);
+    CoverageTool2000.initCoverageMatrix(1, 18);
+  }
+  /**
+   * One time teardown after all the tests are run.
+   */
+  @AfterClass
+  public static void teardown() {
+    System.out.println(CoverageTool2000.checkCoverage(0));
+  }
+}
