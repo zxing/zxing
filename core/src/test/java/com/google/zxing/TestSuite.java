6094ce1f955db0cdce01decb05bc95a56ce9d343
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-02-13 16:15:14 +0100

    [FIX] Fixed placeholder content close #24

--- core/src/test/java/com/google/zxing/TestSuite.java
+++ core/src/test/java/com/google/zxing/TestSuite.java
@@ -20,7 +20,7 @@ public class TestSuite extends TestCase {
   public static void setup() {
     CoverageTool2000.initCoverageMatrix(0, 24);
     CoverageTool2000.initCoverageMatrix(1, 18);
-    CoverageTool2000.initCoverageMatrix(5, TODO);
+    CoverageTool2000.initCoverageMatrix(5, 51);
   }
   /**
    * One time teardown after all the tests are run.
