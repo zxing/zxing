fcece4153b219fef5c6a43d864bbe97cb9695217
Alex Geller <ag@4js.com>
2021-10-20 15:41:44 +0200

    Changed whitespace

--- core/src/main/java/com/google/zxing/qrcode/encoder/MinimalEncoder.java
+++ core/src/main/java/com/google/zxing/qrcode/encoder/MinimalEncoder.java
@@ -79,31 +79,31 @@ final class MinimalEncoder {
   // List of encoders that potentially encode characters not in ISO-8859-1 in one byte.
   private static final List<CharsetEncoder> ENCODERS = new ArrayList<>();
   static {
-    final String[] names = {"ISO-8859-2",
-                            "ISO-8859-3",
-                            "ISO-8859-4",
-                            "ISO-8859-5",
-                            "ISO-8859-6",
-                            "ISO-8859-7",
-                            "ISO-8859-8",
-                            "ISO-8859-9",
-                            "ISO-8859-10",
-                            "ISO-8859-11",
-                            "ISO-8859-13",
-                            "ISO-8859-14",
-                            "ISO-8859-15",
-                            "ISO-8859-16",
-                            "windows-1250",
-                            "windows-1251",
-                            "windows-1252",
-                            "windows-1253",
-                            "windows-1254",
-                            "windows-1255",
-                            "windows-1256",
-                            "windows-1257",
-                            "windows-1258",
-                            "Shift_JIS"};
-    for (String name: names) {
+    final String[] names = { "ISO-8859-2",
+                             "ISO-8859-3",
+                             "ISO-8859-4",
+                             "ISO-8859-5",
+                             "ISO-8859-6",
+                             "ISO-8859-7",
+                             "ISO-8859-8",
+                             "ISO-8859-9",
+                             "ISO-8859-10",
+                             "ISO-8859-11",
+                             "ISO-8859-13",
+                             "ISO-8859-14",
+                             "ISO-8859-15",
+                             "ISO-8859-16",
+                             "windows-1250",
+                             "windows-1251",
+                             "windows-1252",
+                             "windows-1253",
+                             "windows-1254",
+                             "windows-1255",
+                             "windows-1256",
+                             "windows-1257",
+                             "windows-1258",
+                             "Shift_JIS" };
+    for (String name : names) {
       if (CharacterSetECI.getCharacterSetECIByName(name) != null) {
         try {
           ENCODERS.add(Charset.forName(name).newEncoder());
