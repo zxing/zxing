21eb978d6732714751ffee4c74edb8479260340f
AlexGeller1 <87009702+AlexGeller1@users.noreply.github.com>
2021-11-16 01:25:31 +0100

    Fix of error in specification ISO/IEC 16022 Second edition Annex P as suggested by Martin Burke of zint (See https://sourceforge.net/p/zint/code/ci/68566fefd2a4623041b0232a5941f8fed1e22018/) (#1459)
    Quote of Martin's proposed changes to the spec:
    The algorithm can be salvaged to be at least sound by making those changes, e.g. by adding to step e):
    
     "1) If the next character cannot be encoded as X12, switch to ASCII mode and go to step b)."
    
    (and renumbering the following sub-steps). Similarly step f):
    
    "1) If the next character cannot be encoded as EDIFACT, switch to ASCII mode and go to step b)."

--- core/src/main/java/com/google/zxing/datamatrix/encoder/HighLevelEncoder.java
+++ core/src/main/java/com/google/zxing/datamatrix/encoder/HighLevelEncoder.java
@@ -193,6 +193,26 @@ public final class HighLevelEncoder {
   }
 
   static int lookAheadTest(CharSequence msg, int startpos, int currentMode) {
+    int newMode = lookAheadTestIntern(msg, startpos, currentMode);
+    if (currentMode == X12_ENCODATION && newMode == X12_ENCODATION) {
+      int endpos = Math.min(startpos + 3, msg.length());
+      for (int i = startpos; i < endpos; i++) {
+        if (!isNativeX12(msg.charAt(i))) {
+          return ASCII_ENCODATION;
+        }
+      }
+    } else if (currentMode == EDIFACT_ENCODATION && newMode == EDIFACT_ENCODATION) {
+      int endpos = Math.min(startpos + 4, msg.length());
+      for (int i = startpos; i < endpos; i++) {
+        if (!isNativeEDIFACT(msg.charAt(i))) {
+          return ASCII_ENCODATION;
+        }
+      }
+    }
+    return newMode;
+  }
+
+  static int lookAheadTestIntern(CharSequence msg, int startpos, int currentMode) {
     if (startpos >= msg.length()) {
       return currentMode;
     }
