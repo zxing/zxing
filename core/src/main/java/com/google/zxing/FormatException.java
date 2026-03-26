78db5a6b077df1766ddc049111e7e1e2f9cd813d
albert-github <albert.tests@gmail.com>
2026-03-26 11:54:00 +0100

    Spelling correction
    Spelling correction

--- core/src/main/java/com/google/zxing/FormatException.java
+++ core/src/main/java/com/google/zxing/FormatException.java
@@ -19,7 +19,7 @@ package com.google.zxing;
 /**
  * Thrown when a barcode was successfully detected, but some aspect of
  * the content did not conform to the barcode's format rules. This could have
- * been due to a mis-detection.
+ * been due to a missed or incorrect detection.
  *
  * @author Sean Owen
  */
